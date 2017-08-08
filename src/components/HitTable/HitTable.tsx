61dd15d0306c11332ad51077effcdfdc6ea7d9b5
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-08 02:47:29 -0400

    HitTable is no longer responsible for querying Turkopticon

--- src/components/HitTable/HitTable.tsx
+++ src/components/HitTable/HitTable.tsx
@@ -2,21 +2,12 @@ import * as React from 'react';
 import { Hit } from '../../types';
 import { ResourceList } from '@shopify/polaris';
 import HitCard from './HitCard';
-import { filterHitsWithoutTO } from '../../utils/turkopticon';
 
 export interface Props {
   readonly hits: Hit[];
 }
 
-export interface Handlers {
-  onRefresh: (hits: Hit[]) => void;
-}
-
-class HitTable extends React.PureComponent<Props & Handlers, never> {
-  componentWillReceiveProps() {
-    this.props.onRefresh(filterHitsWithoutTO(this.props.hits));
-  }
-
+class HitTable extends React.PureComponent<Props, never> {
   public render() {
     return (
       <ResourceList
