3ed5b7bd94524839c3bc2908458c90c2a69bc35f
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-09 02:21:04 -0400

    FETCH_TURKOPTICON_SUCCESS now triggers the hit reducer to use the action data paylod to update hits  corresponding to the requester with the requester in its 'turkopticon' field.

--- src/reducers/hits.ts
+++ src/reducers/hits.ts
@@ -1,17 +1,28 @@
 import { Hit, HitSet } from '../types';
 import { HitPageAction } from '../actions/hits';
-import { FETCH_HIT_PAGE_SUCCESS } from '../constants';
+import { TOpticonAction } from '../actions/turkopticon';
+import { FETCH_HIT_PAGE_SUCCESS, FETCH_TURKOPTICON_SUCCESS } from '../constants';
 import { Set } from 'immutable';
-import sampleHits from '../utils/sampleHits';
+// import sampleHits from '../utils/sampleHits';
 
-const initial: HitSet = Set<Hit>(sampleHits);
+const initial: HitSet = Set<Hit>([]);
 
-export default (state = initial, action: HitPageAction): HitSet => {
+type FetchAction = HitPageAction | TOpticonAction;
+
+export default (state = initial, action: FetchAction): HitSet => {
   let partialState: HitSet | undefined;
 
   switch (action.type) {
     case FETCH_HIT_PAGE_SUCCESS:
       partialState = action.data;
+      break;
+    case FETCH_TURKOPTICON_SUCCESS:
+      partialState = state
+        .map((hit: Hit): Hit => ({
+          ...hit,
+          ...{ turkopticon: action.data.get(hit.requesterId) || hit.turkopticon }
+        }))
+        .toSet();
       break;
     default:
       return state;
