bdcef01d37ad23add978a6892100c8f3a70eda42
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-09 19:41:13 -0400

    Truncate now accepts an optional max string length parameter

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -3,7 +3,7 @@
  * characters after character 90 with an ellipsis.
  * @param str
  */
-export const truncate = (str: string): string => {
+export const truncate = (str: string, max = 90): string => {
   const trimmed = str.trim();
-  return trimmed.length > 90 ? trimmed.slice(0, 89).concat(' ...') : trimmed;
+  return trimmed.length > max ? trimmed.slice(0, 89).concat(' ...') : trimmed;
 };
