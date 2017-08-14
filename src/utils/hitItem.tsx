7d524b0672a5c3204a983813b3b57349858259c3
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-14 03:51:01 -0400

    Reorder attributes

---
+++ src/utils/hitItem.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { Link } from '@shopify/polaris';
+import { Hit, Requester } from '../types';
+import { generateBadges } from './badges';
+import { truncate } from './formatting';
+import { tutkopticonBaseUrl } from '../constants';
+
+type ExceptionStatus = 'neutral' | 'warning' | 'critical';
+export interface ExceptionDescriptor {
+  status?: ExceptionStatus;
+  title?: string;
+  description?: string;
+}
+
+const generateExceptions = (groupId: string): ExceptionDescriptor[] => {
+  return groupId.startsWith('[Error:groupId]-')
+    ? [ { status: 'warning', title: 'You are not qualified.' } ]
+    : [];
+};
+
+export const requesterButton = (requester: Requester | string, id: string) => {
+  const textNoTO = (name: string) => {
+    return <p>{truncate(name, 45)}</p>;
+  };
+
+  const buttonTO = (props: Requester) => {
+    const { name } = props;
+
+    return (
+      <Link url={tutkopticonBaseUrl + id} external>
+        {`${truncate(name, 45)}`}
+      </Link>
+    );
+  };
+
+  return typeof requester === 'string' ? textNoTO(requester) : buttonTO(requester);
+};
+
+export const generateItemProps = (hit: Hit, requester: Requester | undefined) => {
+  const { requesterName, reward, groupId, title } = hit;
+
+  const actions = [
+    {
+      content: 'Preview',
+      external: true,
+      url: `https://www.mturk.com/mturk/preview?groupId=${groupId}`
+    },
+    {
+      content: 'Accept',
+      primary: true,
+      external: true,
+      url: `https://www.mturk.com/mturk/previewandaccept?groupId=${groupId}`
+    }
+  ];
+
+  return {
+    attributeOne: requesterName,
+    attributeTwo: truncate(title, 80),
+    // attributeThree: requesterButton(requester || requesterName, requesterId),
+    attributeThree: `$${reward}`,
+    badges: generateBadges(requester),
+    actions,
+    exceptions: generateExceptions(groupId)
+  };
+};
