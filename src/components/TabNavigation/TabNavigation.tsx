7834c3b8c4ca71feb8f8403b42688408c8724492
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-16 21:34:23 -0400

    Add TabNavigation component.

--- src/components/TabNavigation/TabNavigation.tsx
+++ src/components/TabNavigation/TabNavigation.tsx
@@ -1,31 +1,34 @@
-// import * as React from 'react'
-// import {Tabs} from '@shopify/polaris';
-// import { HitMap, RequesterMap, } from '../../types';
-// import HitTable from '../HitTable/HitTable';
-// import Search from '../../containers/Search';
-// import { tabs } from '../../utils/tabs';
+import * as React from 'react';
+import { Tabs, Stack } from '@shopify/polaris';
+import { HitMap, RequesterMap, SearchOptions } from '../../types';
+import HitTable from '../HitTable/HitTable';
+import Search from '../../containers/Search';
+import { tabs } from '../../utils/tabs';
 
-// export interface Props {
-//   readonly selected: number;
-//   readonly hits: HitMap;
-//   readonly requesters: RequesterMap;
-// }
+export interface Props {
+  readonly selected: number;
+  readonly hits: HitMap;
+  readonly requesters: RequesterMap;
+  readonly options: SearchOptions;
+}
 
-// export interface Handlers {
-//   readonly onSelectTab: (selectedTabIndex: number) => void;
-// }
+export interface Handlers {
+  readonly onSelectTab: (selectedTabIndex: number) => void;
+  readonly onFetch: (options: SearchOptions) => void;
+}
 
-// const TabNavigation = (props: Props & Handlers) => {
-//   const { onSelectTab, selected, hits, requesters } = props;
-//   return (
-//           <Tabs selected={selected} tabs={tabs} onSelect={onSelectTab}>
-//             <Search />
-//             <HitTable
-//               hits={hits}
-//               requesters={requesters}
-//             />
-//           </Tabs>
-//   );
-// };
+const TabNavigation = (props: Props & Handlers) => {
+  const { onSelectTab, onFetch, selected, hits, requesters, options } = props;
+  const fetchAction = () => onFetch(options);
 
-// export default App;
+  return (
+    <Tabs selected={selected} tabs={tabs} onSelect={onSelectTab}>
+      <Stack vertical spacing="loose">
+        <Search />
+        <HitTable hits={hits} requesters={requesters} emptyAction={fetchAction} />
+      </Stack>
+    </Tabs>
+  );
+};
+
+export default TabNavigation;
