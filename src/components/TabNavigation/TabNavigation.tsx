8be5c252f0d30453d8623a337da97c408adfc39a
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-16 21:40:38 -0400

    Reduce responsibility of TabNavigation component

--- src/components/TabNavigation/TabNavigation.tsx
+++ src/components/TabNavigation/TabNavigation.tsx
@@ -1,31 +1,24 @@
 import * as React from 'react';
 import { Tabs, Stack } from '@shopify/polaris';
-import { HitMap, RequesterMap, SearchOptions } from '../../types';
 import HitTable from '../HitTable/HitTable';
 import Search from '../../containers/Search';
 import { tabs } from '../../utils/tabs';
 
 export interface Props {
   readonly selected: number;
-  readonly hits: HitMap;
-  readonly requesters: RequesterMap;
-  readonly options: SearchOptions;
 }
 
 export interface Handlers {
   readonly onSelectTab: (selectedTabIndex: number) => void;
-  readonly onFetch: (options: SearchOptions) => void;
 }
 
 const TabNavigation = (props: Props & Handlers) => {
-  const { onSelectTab, onFetch, selected, hits, requesters, options } = props;
-  const fetchAction = () => onFetch(options);
+  const { onSelectTab, selected } = props;
 
   return (
     <Tabs selected={selected} tabs={tabs} onSelect={onSelectTab}>
       <Stack vertical spacing="loose">
         <Search />
-        <HitTable hits={hits} requesters={requesters} emptyAction={fetchAction} />
       </Stack>
     </Tabs>
   );
