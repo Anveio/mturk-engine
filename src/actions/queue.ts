3b2df12f55bc51c13e15e873e3de7a2124358c84
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-17 10:41:22 -0400

    Add fetch queue failure actions.

--- src/actions/queue.ts
+++ src/actions/queue.ts
@@ -1,14 +1,22 @@
-import * as constants from '../constants';
+import { FETCH_QUEUE_FAILURE, FETCH_QUEUE_SUCCESS } from '../constants';
 import { HitMap } from '../types';
 
 export interface FetchQueueSuccess {
-  type: constants.FETCH_QUEUE_SUCCESS;
+  type: FETCH_QUEUE_SUCCESS;
   data: HitMap;
+}
+
+export interface FetchQueueFailure {
+  type: FETCH_QUEUE_FAILURE;
 }
 
 export type QueueAction = FetchQueueSuccess;
 
 export const fetchQueueSuccess = (data: HitMap): FetchQueueSuccess => ({
-  type: constants.FETCH_QUEUE_SUCCESS,
+  type: FETCH_QUEUE_SUCCESS,
   data
+});
+
+export const fetchQueueFailure = (): FetchQueueFailure => ({
+  type: FETCH_QUEUE_FAILURE
 });
