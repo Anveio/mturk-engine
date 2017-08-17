7ed55093d204eacdb4cb9cd210616bb341cda921
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-16 21:40:55 -0400

    Add TabNavigation container

---
+++ src/containers/TabNavigation.ts
@@ -0,0 +1,19 @@
+import { connect, Dispatch } from 'react-redux';
+import { RootState } from '../types';
+import { changeTab, ChangeTab } from '../actions/tab';
+import TabNavigation, {
+  Props,
+  Handlers
+} from '../components/TabNavigation/TabNavigation';
+
+const mapState = (state: RootState): Props => ({
+  selected: state.tab
+});
+
+const mapDispatch = (dispatch: Dispatch<ChangeTab>): Handlers => ({
+  onSelectTab: (selectedTabIndex: number) => {
+    dispatch(changeTab(selectedTabIndex));
+  }
+});
+
+export default connect(mapState, mapDispatch)(TabNavigation);
