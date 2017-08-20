0696067f6cc0ef240cb6dabab59a7050d1a20a50
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-20 09:02:04 -0400

    Remove debugging code and finish first draft of accepting HITs via XHR feature.

--- src/requests/acceptHit.ts
+++ src/requests/acceptHit.ts
@@ -1,14 +1,12 @@
 import { Dispatch } from 'react-redux';
 import { AcceptAction, acceptHitSuccess, acceptHitFailure } from '../actions/accept';
-// import { validateHitAcceptRequest } from '../utils/acceptHit';
+import { validateHitAcceptRequest } from '../utils/acceptHit';
 
 export const sendAcceptRequest = (dispatch: Dispatch<AcceptAction>) => async (
   groupId: string
 ) => {
   try {
-    console.log('hi');
-    // const successful = await validateHitAcceptRequest(groupId);
-    const successful = true;
+    const successful = await validateHitAcceptRequest(groupId);
     successful ? dispatch(acceptHitSuccess()) : dispatch(acceptHitFailure());
     return successful;
   } catch (e) {
