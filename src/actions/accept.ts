490395c3acba8fc27967138bc5474a2f9901a3b8
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-20 08:32:54 -0400

    Add actions and action types for accept hit failure.

--- src/actions/accept.ts
+++ src/actions/accept.ts
@@ -1,11 +1,18 @@
-import { ACCEPT_HIT_SUCCESS } from '../constants';
+import { ACCEPT_HIT_SUCCESS, ACCEPT_HIT_FAILURE } from '../constants';
 
 export interface AcceptHitSuccess {
   type: ACCEPT_HIT_SUCCESS;
 }
+export interface AcceptHitFailure {
+  type: ACCEPT_HIT_FAILURE;
+}
 
-export type AcceptAction = AcceptHitSuccess;
+export type AcceptAction = AcceptHitSuccess | AcceptHitFailure;
 
 export const acceptHitSuccess = (): AcceptHitSuccess => ({
   type: ACCEPT_HIT_SUCCESS
+});
+
+export const acceptHitFailure = (): AcceptHitFailure => ({
+  type: ACCEPT_HIT_FAILURE
 });
--- src/requests/acceptHit.ts
+++ src/requests/acceptHit.ts
@@ -1,25 +1,16 @@
 import { Dispatch } from 'react-redux';
-import { SearchItem } from '../types';
-import { Map } from 'immutable';
-import { AcceptAction, acceptHitSuccess } from '../actions/accept';
-import { validateHitAcceptRequest } from '../utils/acceptHit'
+import { AcceptAction, acceptHitSuccess, acceptHitFailure } from '../actions/accept';
+import { validateHitAcceptRequest } from '../utils/acceptHit';
 
 export const sendAcceptRequest = (dispatch: Dispatch<AcceptAction>) => async (
   groupId: string
 ) => {
-  const fetchHits = (async () => {
-    try {
-      const successful = await validateHitAcceptRequest(groupId);
-      successful
-        ? dispatch(acceptHitSuccess())
-        : dispatch(acceptHitFailure());
-      return successful
-    } catch (e) {
-      /**
-       * Return an empty set on error to simplify function signature.
-       */
-      dispatch(acceptHitFailure());
-      return false;
-    }
-  })();
+  try {
+    const successful = await validateHitAcceptRequest(groupId);
+    successful ? dispatch(acceptHitSuccess()) : dispatch(acceptHitFailure());
+    return successful;
+  } catch (e) {
+    dispatch(acceptHitFailure());
+    return false;
+  }
 };
