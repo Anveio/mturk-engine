33cf7e813fa8dd3611149449ffa70bad68342be6
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-21 06:05:05 -0400

    Toastr now displays after waiting for network request to resolve.

--- src/requests/acceptHit.ts
+++ src/requests/acceptHit.ts
@@ -1,14 +1,14 @@
 import { Dispatch } from 'react-redux';
 import { AcceptAction, acceptHitSuccess, acceptHitFailure } from '../actions/accept';
-// import { validateHitAcceptRequest } from '../utils/acceptHit';
+import { validateHitAcceptRequest } from '../utils/acceptHit';
 import { generateAcceptHitToast } from '../utils/toastr';
 
 export const sendAcceptRequest = (dispatch: Dispatch<AcceptAction>) => async (
   groupId: string
 ) => {
   try {
-    // const successful = await validateHitAcceptRequest(groupId);
-    const successful = true;
+    const successful = await validateHitAcceptRequest(groupId);
+    // const successful = true;
     generateAcceptHitToast(successful, groupId);
     successful ? dispatch(acceptHitSuccess()) : dispatch(acceptHitFailure());
     return successful;
