db49ef0191f7cc56ab1a833648c7d08e1aad3a10
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-24 13:47:16 -0400

    Add functions for loading and unloading state from localForage.

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,5 @@
 import { RootState } from './types';
 import { createStore, compose } from 'redux';
-import { persistStore } from 'redux-persist';
 import { rootReducer } from './reducers';
 
 // tslint:disable:no-any
@@ -10,7 +9,5 @@ const devtools: any = window['devToolsExtension']
   : (f: any) => f;
 
 const store = createStore<RootState>(rootReducer, compose(devtools()));
-
-persistStore<RootState>(store);
 
 export default store;
