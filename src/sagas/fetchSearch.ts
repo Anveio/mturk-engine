028d91621b02fee7536f1d8689113e57547b334e
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-25 13:39:43 -0400

    Improve error handling of fetchSearch.

--- src/sagas/fetchSearch.ts
+++ src/sagas/fetchSearch.ts
@@ -1,4 +1,6 @@
 import { call, put } from 'redux-saga/effects';
+import { SearchItem } from '../types';
+import { Map } from 'immutable';
 import {
   SearchRequest,
   SearchFailure,
@@ -6,13 +8,37 @@ import {
   searchSuccess,
   SearchSuccess
 } from '../actions/search';
+import {
+  FetchTOpticonSuccess,
+  FetchTOpticonFailure,
+  fetchTOpticonSuccess,
+  fetchTOpticonFailure
+} from '../actions/turkopticon';
 import { batchFetchHits } from '../utils/fetchHits';
+import { batchFetchTOpticon, requesterIdsWithNoTO } from '../utils/turkopticon';
+import { generateSearchToast } from '../utils/toastr';
 
 export function* fetchSearch(action: SearchRequest) {
   try {
-    const hitData = yield call(batchFetchHits, action.options);
-    yield put<SearchSuccess>(searchSuccess(hitData));
+    const hitData: Map<string, SearchItem> = yield call(
+      batchFetchHits,
+      action.options
+    );
+
+    const empty = hitData.isEmpty();
+    generateSearchToast(!empty);
+    empty
+      ? yield put<SearchFailure>(searchFailure())
+      : yield put<SearchSuccess>(searchSuccess(hitData));
+
+    if (!empty) {
+      const requesterIds = requesterIdsWithNoTO(hitData);
+      const topticonData = yield call(batchFetchTOpticon, requesterIds);
+      yield put<FetchTOpticonSuccess>(fetchTOpticonSuccess(topticonData));
+    }
   } catch (e) {
+    generateSearchToast(false);
     yield put<SearchFailure>(searchFailure());
+    yield put<FetchTOpticonFailure>(fetchTOpticonFailure());
   }
 }
