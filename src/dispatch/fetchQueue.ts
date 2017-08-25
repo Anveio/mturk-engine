138cb98ff9adb67e7905b29730677eb422ef631d
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-25 11:53:10 -0400

    Temporarily remove Toastr.

--- src/dispatch/fetchQueue.ts
+++ src/dispatch/fetchQueue.ts
@@ -1,20 +1,29 @@
-import { Dispatch } from 'react-redux';
+// import { Dispatch } from 'react-redux';
 import { SearchItem } from '../types';
 import { Map } from 'immutable';
-import { fetchQueueSuccess, fetchQueueFailure, QueueAction } from '../actions/queue';
+// import { fetchQueueSuccess, fetchQueueFailure, QueueAction } from '../actions/queue';
 import { getQueuePage } from '../utils/fetchQueue';
-import { generateQueueToast, failedQueueToast } from '../utils/toastr';
+// import { generateQueueToast, failedQueueToast } from '../utils/toastr';
 
-export const fetchQueue = (dispatch: Dispatch<QueueAction>) => async () => {
+// export const fetchQueue = (dispatch: Dispatch<QueueAction>) => async () => {
+//   try {
+//     const queueData = await getQueuePage();
+//     const empty = !queueData.isEmpty();
+//     generateQueueToast(empty);
+//     dispatch(fetchQueueSuccess(queueData));
+//     return queueData;
+//   } catch (e) {
+//     dispatch(fetchQueueFailure());
+//     failedQueueToast();
+//     return Map<string, SearchItem>();
+//   }
+// };
+
+export const fetchQueue = async () => {
   try {
     const queueData = await getQueuePage();
-    const empty = !queueData.isEmpty();
-    generateQueueToast(empty);
-    dispatch(fetchQueueSuccess(queueData));
     return queueData;
   } catch (e) {
-    dispatch(fetchQueueFailure());
-    failedQueueToast();
     return Map<string, SearchItem>();
   }
 };
