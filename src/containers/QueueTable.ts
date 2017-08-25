a9ed1858307d9745987887963fc227787f81f7a1
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-25 11:52:53 -0400

    QueueTable now only dispatches requests for fetching queue.

--- src/containers/QueueTable.ts
+++ src/containers/QueueTable.ts
@@ -1,8 +1,8 @@
 import { connect, Dispatch } from 'react-redux';
 import { RootState } from '../types';
-import { QueueAction } from '../actions/queue';
+import { QueueAction, fetchQueueRequest } from '../actions/queue';
 import QueueTable, { Props, Handlers } from '../components/Queue/QueueTable';
-import { fetchQueue } from '../dispatch/fetchQueue';
+// import { fetchQueue } from '../dispatch/fetchQueue';
 import { returnHit } from '../dispatch/returnHit';
 
 const mapState = (state: RootState): Props => ({
@@ -10,7 +10,7 @@ const mapState = (state: RootState): Props => ({
 });
 
 const mapDispatch = (dispatch: Dispatch<QueueAction>): Handlers => ({
-  onRefresh: fetchQueue(dispatch),
+  onRefresh: () => dispatch(fetchQueueRequest()),
   onReturn: returnHit(dispatch)
 });
 
