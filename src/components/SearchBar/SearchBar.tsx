7be88875860bfad0e9b2cf7ec37d8dd0f5ce1a8a
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-31 13:33:45 -0400

    Simplify SearchBar to be purely presentational component.

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,42 +1,16 @@
 import * as React from 'react';
-import { Card, Collapsible } from '@shopify/polaris';
-import { SearchOptions } from '../../types';
+import { Card } from '@shopify/polaris';
 import SearchSettings from '../../containers/SearchSettings';
 import SearchButtons from '../../containers/SearchButtons';
 
-export interface Handlers {
-  readonly onToggleSettings: () => void;
-  readonly onSearch: (options: SearchOptions) => void;
-}
-
-export interface Props {
-  readonly settingsActive: boolean;
-  readonly searchActive: boolean;
-  readonly options: SearchOptions;
-}
-
-const SearchBar = (props: Props & Handlers) => {
-  const { settingsActive, options, onSearch } = props;
-
-  const watchForEnter = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (event.charCode === 13) {
-      event.preventDefault();
-      onSearch(options);
-    }
-  };
-
+const SearchBar = () => {
   return (
     <Card>
       <Card.Section>
         <SearchButtons />
       </Card.Section>
-      <Collapsible open={settingsActive}>
-        <Card.Section>
-          <div onKeyPress={watchForEnter}>
-            <SearchSettings />
-          </div>
-        </Card.Section>
-      </Collapsible>
+
+      <SearchSettings />
     </Card>
   );
 };
