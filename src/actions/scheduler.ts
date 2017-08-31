f51961bd47168f3557486e793dd4ecb1e424cb4b
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-31 16:18:41 -0400

    Add reducer for scheduling the next search.

--- src/actions/scheduler.ts
+++ src/actions/scheduler.ts
@@ -1,9 +1,21 @@
-import { TOGGLE_SEARCH_ACTIVITY } from '../constants';
+import { SCHEDULE_NEXT_SEARCH, CANCEL_NEXT_SEARCH } from '../constants';
 
-export interface ToggleSearchActive {
-  type: TOGGLE_SEARCH_ACTIVITY;
+export interface ScheduleNextSearch {
+  type: SCHEDULE_NEXT_SEARCH;
+  time: Date;
 }
 
-export const toggleSearchActive = (): ToggleSearchActive => ({
-  type: TOGGLE_SEARCH_ACTIVITY
+export interface CancelNextSearch {
+  type: CANCEL_NEXT_SEARCH;
+}
+
+export type ScheduleAction = ScheduleNextSearch | CancelNextSearch;
+
+export const scheduleSearch = (time: Date): ScheduleNextSearch => ({
+  type: SCHEDULE_NEXT_SEARCH,
+  time
+});
+
+export const cancelNextSearch = (): CancelNextSearch => ({
+  type: CANCEL_NEXT_SEARCH
 });
--- src/reducers.ts
+++ src/reducers.ts
@@ -6,6 +6,7 @@ import { default as searchingActive } from './reducers/searchingActive';
 import { default as search } from './reducers/search';
 import { default as queue } from './reducers/queue';
 import { default as requesters } from './reducers/requesters';
+import { default as scheduler } from './reducers/scheduler';
 import { default as searchOptions } from './reducers/searchOptions';
 import { default as searchFormActive } from './reducers/searchFormActive';
 import { default as sortingOption } from './reducers/sortingOption';
@@ -17,6 +18,7 @@ export const rootReducer = combineReducers<RootState>({
   queue,
   search,
   toastr,
+  scheduler,
   requesters,
   hitBlocklist,
   searchOptions,
