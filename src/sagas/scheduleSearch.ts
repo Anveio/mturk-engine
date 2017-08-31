90cbecf3aadb633682fe694d684f8cd8a616a1c4
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-31 17:41:55 -0400

    Clicking the 'Start searching' will continually send search requests.

--- src/sagas/scheduleSearch.ts
+++ src/sagas/scheduleSearch.ts
@@ -1,11 +1,11 @@
 import { delay } from 'redux-saga';
+import { put } from 'redux-saga/effects';
+import { SearchRequest, searchRequest } from '../actions/search';
 import { ScheduleNextSearch } from '../actions/scheduler';
 
 export function* searchAfterDelay(action: ScheduleNextSearch) {
-  console.log(Math.abs(action.time.valueOf() - Date.now()));
-  yield delay(action.time.valueOf() - Date.now());
-  console.log('this was a delayed action');
   try {
+    yield delay(action.time.valueOf() - Date.now());
+    yield put<SearchRequest>(searchRequest());
   } catch (e) {}
 }
-
