c1ef7363d354ab48471e0b0342254cf1cdadd288
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-04 19:14:00 -0400

    Next scheduled search has a minimum wait time of 1 second.

--- src/sagas/scheduleSearch.ts
+++ src/sagas/scheduleSearch.ts
@@ -5,7 +5,7 @@ import { SearchRequest, searchRequest } from '../actions/search';
 import { ScheduleNextSearch } from '../actions/scheduler';
 
 export function* searchAfterDelay(action: ScheduleNextSearch) {
-  yield delay(action.time.valueOf() - Date.now());
+  yield delay(Math.max(action.time.valueOf() - Date.now()), 1000);
   const searchingActive = yield select(
     (state: RootState) => state.searchingActive
   );
