be79878ddaa269810bfa7e7c8ad58441941a1774
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-05 03:41:56 -0400

    SearchSettings is now a PureComponent.

--- src/components/SearchBar/SearchSettings.tsx
+++ src/components/SearchBar/SearchSettings.tsx
@@ -25,57 +25,62 @@ export interface Handlers {
 }
 
 export interface Props {
-  searchOptions: SearchOptions;
-  formActive: boolean;
+  readonly searchOptions: SearchOptions;
+  readonly formActive: boolean;
 }
 
-const SearchSettings = (props: Props & Handlers) => {
-  const { onChange, onSearch, formActive } = props;
-  const { delay, minReward, sortType, qualified } = props.searchOptions;
-
-  const updateField = (field: keyof SearchOptions) => (value: string) => {
-    onChange(field, value);
+class SearchSettings extends React.PureComponent<Props & Handlers, never> {
+  private updateField = (field: keyof SearchOptions) => (value: string) => {
+    this.props.onChange(field, value);
   };
 
-  const watchForEnter = (event: React.KeyboardEvent<HTMLDivElement>) => {
+  private toggleField = (field: keyof SearchOptions) => (value: boolean) => {
+    this.props.onChange(field, value);
+  };
+
+  private watchForEnter = (event: React.KeyboardEvent<HTMLDivElement>) => {
     if (event.charCode === 13) {
       event.preventDefault();
-      onSearch();
+      this.props.onSearch();
     }
   };
 
-  const toggleField = (field: keyof SearchOptions) => (value: boolean) => {
-    onChange(field, value);
-  };
+  public render() {
+    const { delay, minReward, sortType, qualified } = this.props.searchOptions;
 
-  return (
-    <Collapsible open={formActive}>
-      <Card.Section>
-        <div onKeyPress={watchForEnter}>
-          <FormLayout>
-            <DisplayText size="small">Edit search settings.</DisplayText>
-            <Caption>
-              Changes are saved as you type and will apply on your next search.
-            </Caption>
-            <SearchDelayField onChange={updateField('delay')} value={delay} />
-            <MinimumRewardField
-              onChange={updateField('minReward')}
-              value={minReward}
-            />
-            <SortTypeField
-              onChange={updateField('sortType')}
-              value={sortType}
-              options={sortingOptions}
-            />
-            <QualifiedBox
-              onChange={toggleField('qualified')}
-              checked={qualified}
-            />
-          </FormLayout>
-        </div>
-      </Card.Section>
-    </Collapsible>
-  );
-};
+    return (
+      <Collapsible open={this.props.formActive}>
+        <Card.Section>
+          <div onKeyPress={this.watchForEnter}>
+            <FormLayout>
+              <DisplayText size="small">Edit search settings.</DisplayText>
+              <Caption>
+                Changes are saved as you type and will apply on your next
+                search.
+              </Caption>
+              <SearchDelayField
+                onChange={this.updateField('delay')}
+                value={delay}
+              />
+              <MinimumRewardField
+                onChange={this.updateField('minReward')}
+                value={minReward}
+              />
+              <SortTypeField
+                onChange={this.updateField('sortType')}
+                value={sortType}
+                options={sortingOptions}
+              />
+              <QualifiedBox
+                onChange={this.toggleField('qualified')}
+                checked={qualified}
+              />
+            </FormLayout>
+          </div>
+        </Card.Section>
+      </Collapsible>
+    );
+  }
+}
 
 export default SearchSettings;
