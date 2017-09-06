9245f51b2992e2201034738cb66c9e5723610065
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-06 00:19:32 -0400

    Add persistence for hitBlocklist.

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,9 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-// import { autoRehydrate, createTransform, persistStore } from 'redux-persist';
-// import * as localForage from 'localforage';
-// import { fromJS } from 'immutable';
-// import { HitBlockMap } from './types';
+import * as immutableTransform from 'redux-persist-transform-immutable';
+import { autoRehydrate, persistStore } from 'redux-persist';
+import * as localForage from 'localforage';
+
 import { rootReducer } from './reducers';
 import rootSaga from './sagas';
 
@@ -17,27 +17,15 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore<any>(
   rootReducer,
-  compose(applyMiddleware(sagaMiddleware), devtools)
+  compose(applyMiddleware(sagaMiddleware), autoRehydrate(), devtools)
 );
 
 sagaMiddleware.run(rootSaga);
 
-// const immutableTransform = createTransform<HitBlockMap, any>(
-//   (inboundState: HitBlockMap, key: string) => {
-//     console.log(inboundState, `key inbound: ${key}`);
-//     return inboundState.toJS();
-//   },
-//   (outboundState: HitBlockMap, key: string) => {
-//     console.log(fromJS(outboundState), `key outbound: ${key}`);
-//     return fromJS(outboundState);
-//   },
-//   { whitelist: [ 'hitBlocklist' ] }
-// );
-
-// persistStore(store, {
-//   whitelist: [ 'hitBlocklist' ],
-//   storage: localForage,
-//   transforms: [ immutableTransform ]
-// });
+persistStore(store, {
+  whitelist: [ 'hitBlocklist' ],
+  storage: localForage,
+  transforms: [ immutableTransform({ whitelist: [ 'hitBlocklist' ] }) ]
+});
 
 export default store;
