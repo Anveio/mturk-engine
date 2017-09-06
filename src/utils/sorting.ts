39eb02bfacfd5dd38d3c70caffcd1d6f66502816
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-05 22:46:10 -0400

    Create generalized sort function to handle needing ascending and descending sort under different contexts.

--- src/utils/sorting.ts
+++ src/utils/sorting.ts
@@ -1,5 +1,7 @@
 import { SearchResult, SortingOption } from '../types';
 
+type SortOrder = 'ascending' | 'descending';
+
 const optionsMap = {
   'Batch Size': 'batchSize',
   Reward: 'reward',
@@ -7,7 +9,26 @@ const optionsMap = {
   default: 'index'
 };
 
+interface SortOrderMap {
+  [key: string]: SortOrder;
+}
+
+const sortOrderMap: SortOrderMap = {
+  'Batch Size': 'descending',
+  Reward: 'descending',
+  Latest: 'ascending',
+  default: 'ascending'
+};
+
+const calculateSortOrder = (option: SortingOption): SortOrder =>
+  sortOrderMap[option];
+
 export const sortBy = (option: SortingOption) => {
   const property = optionsMap[option] || optionsMap.default;
-  return (a: SearchResult, b: SearchResult) => +b[property] - +a[property];
+  const sortOrder = calculateSortOrder(option);
+  if (sortOrder === 'descending') {
+    return (a: SearchResult, b: SearchResult) => +b[property] - +a[property];
+  } else {
+    return (a: SearchResult, b: SearchResult) => +a[property] - +b[property];
+  }
 };
