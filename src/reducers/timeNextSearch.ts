050b559e3de62ee4dc6696797a7ffee95ef72a53
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-06 02:33:50 -0400

    CANCEL_NEXT_SEARCH will cause timeNextSearch state to become null.

--- src/reducers/timeNextSearch.ts
+++ src/reducers/timeNextSearch.ts
@@ -1,5 +1,5 @@
 import { ScheduleAction } from '../actions/scheduler';
-import { SCHEDULE_NEXT_SEARCH } from '../constants';
+import { SCHEDULE_NEXT_SEARCH, CANCEL_NEXT_SEARCH } from '../constants';
 
 export default (
   state: Date | null = null,
@@ -8,6 +8,8 @@ export default (
   switch (action.type) {
     case SCHEDULE_NEXT_SEARCH:
       return action.time;
+    case CANCEL_NEXT_SEARCH:
+      return null;
     default:
       return state;
   }
