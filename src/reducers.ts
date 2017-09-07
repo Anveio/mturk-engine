0cc8d340388dfd8e2f13026f7cb104ce5d33285a
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-07 03:07:56 -0400

    Add requesterBlockList to root reducer.

--- src/reducers.ts
+++ src/reducers.ts
@@ -12,6 +12,7 @@ import { default as sortingOption } from './reducers/sortingOption';
 import { default as hitBlocklist } from './reducers/hitBlocklist';
 import { default as timeNextSearch } from './reducers/timeNextSearch';
 import { default as waitingForMturk } from './reducers/waitingForMturk';
+import { default as requesterBlockList } from './reducers/requesterBlockList';
 
 export const rootReducer = combineReducers<RootState>({
   tab,
@@ -25,5 +26,6 @@ export const rootReducer = combineReducers<RootState>({
   timeNextSearch,
   waitingForMturk,
   searchingActive,
-  searchFormActive
+  searchFormActive,
+  requesterBlockList
 });
