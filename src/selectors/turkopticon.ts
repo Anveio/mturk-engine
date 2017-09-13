613c787ff9bb8391d974614e76af609e1d2bac85
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-13 01:41:37 -0400

    Filtering by Minimum TO now respects the settings toggle.

--- src/selectors/turkopticon.ts
+++ src/selectors/turkopticon.ts
@@ -9,20 +9,31 @@ export const minTopticonScoreEnabled = (state: RootState) =>
 export const minWeightedTopticonScore = (state: RootState) =>
   state.topticonSettings.minimumWeightedTO;
 
-/**
- * Keeps HITs that don't have any scores.
- */
 export const filterBelowTOThreshold = createSelector(
-  [ (state: RootState) => state.search, minWeightedTopticonScore ],
-  (hits: SearchResults, minScore: number) =>
-    hits.filter((hit: SearchResult): boolean => {
-      if (!hit.requester.turkopticon) {
-        return true;
-      } else {
-        const averageScore = calculateAverageScore(
-          hit.requester.turkopticon.attrs
-        );
-        return averageScore ? averageScore >= minScore : true;
-      }
-    })
+  [
+    (state: RootState) => state.search,
+    minWeightedTopticonScore,
+    minTopticonScoreEnabled
+  ],
+  (hits: SearchResults, minScore: number, enabled: boolean) => {
+    /**
+     * High complexity code. Todo: separate these into individual selectors and compose them instead of this.
+     * Basically: does nothing if setting isn't enabled, but if it is...
+     * Keeps HITs with TO above minimum score or if it has no reviews at all.
+     */
+    if (enabled) {
+      return hits.filter((hit: SearchResult): boolean => {
+        if (!hit.requester.turkopticon) {
+          return true;
+        } else {
+          const averageScore = calculateAverageScore(
+            hit.requester.turkopticon.attrs
+          );
+          return averageScore ? averageScore >= minScore : true;
+        }
+      });
+    } else {
+      return hits;
+    }
+  }
 );
