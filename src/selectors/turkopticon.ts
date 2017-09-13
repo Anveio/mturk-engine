230ddbbec958098861049e50f50b2f5c628f721e
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-13 02:24:35 -0400

    Add selector for filtering requesters that have no TO. filterBelowTOThreshold now uses this selector.

--- src/selectors/turkopticon.ts
+++ src/selectors/turkopticon.ts
@@ -1,27 +1,36 @@
 import { createSelector } from 'reselect';
 import { RootState, SearchResults, SearchResult } from '../types';
-// import { searchResultSelector } from './searchTable';
 import { calculateAverageScore } from '../utils/turkopticon';
+
+const searchResultSelector = (state: RootState) => state.search;
 
 export const minTopticonScoreEnabled = (state: RootState) =>
   state.topticonSettings.hideBelowThresholdEnabled;
 
+export const hideNoToEnabled = (state: RootState) =>
+  state.topticonSettings.hideNoToEnabled;
+
 export const minWeightedTopticonScore = (state: RootState) =>
   state.topticonSettings.minimumWeightedTO;
 
+export const filterNoTO = createSelector(
+  [ searchResultSelector, hideNoToEnabled ],
+  (hits: SearchResults, enabled: boolean) => {
+    return enabled
+      ? hits.filter((hit: SearchResult) => !!hit.requester.turkopticon)
+      : hits;
+  }
+);
+
 export const filterBelowTOThreshold = createSelector(
-  [
-    (state: RootState) => state.search,
-    minWeightedTopticonScore,
-    minTopticonScoreEnabled
-  ],
-  (hits: SearchResults, minScore: number, enabled: boolean) => {
+  [ filterNoTO, minWeightedTopticonScore, minTopticonScoreEnabled ],
+  (hits: SearchResults, minScore: number, minToEnabled: boolean) => {
     /**
      * High complexity code. Todo: separate these into individual selectors and compose them instead of this.
      * Basically: does nothing if setting isn't enabled, but if it is...
      * Keeps HITs with TO above minimum score or if it has no reviews at all.
      */
-    if (enabled) {
+    if (minToEnabled) {
       return hits.filter((hit: SearchResult): boolean => {
         if (!hit.requester.turkopticon) {
           return true;
