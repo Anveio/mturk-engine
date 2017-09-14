ab80ecf445efc1a0603240f7ab1336ceea1949cb
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-14 10:28:36 -0400

    Add jsdoc annotation.

--- src/redux-persist-transform-immutable.d.ts
+++ src/redux-persist-transform-immutable.d.ts
@@ -1,1 +1,7 @@
 declare module 'redux-persist-transform-immutable';
+
+/**
+ * This is here because `redux-persist-transform-immutable` does not have type
+ * definitions. Not declaring this empty module would cause the TypeScript 
+ * compiler to throw 'implicit any' errors when attempting to use this package.
+ */
