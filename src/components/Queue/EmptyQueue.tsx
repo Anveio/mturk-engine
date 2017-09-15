1b65b3cafbae2c7e908df65dab3b8d8aab949a86
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-15 05:05:14 -0400

    Switch QueueEmptyState with Blueprint non-ideal-state

--- src/components/Queue/EmptyQueue.tsx
+++ src/components/Queue/EmptyQueue.tsx
@@ -1,22 +1,33 @@
 import * as React from 'react';
-import { EmptyState } from '@shopify/polaris';
+import { Button } from '@shopify/polaris';
+import { NonIdealState } from '@blueprintjs/core';
 
 export interface Handlers {
-  onRefresh: () => void;
+  readonly onRefresh: () => void;
 }
 
 const EmptyQueue = ({ onRefresh }: Handlers) => {
   return (
-    <EmptyState
-      heading="Your queue is empty."
-      action={{
-        content: 'Refresh queue',
-        onAction: onRefresh
-      }}
-      image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
-    >
-      <p>Switch to the search tab to accept some HITs.</p>
-    </EmptyState>
+    // <EmptyState
+    //   heading="Your queue is empty."
+    //   action={{
+    //     content: 'Refresh queue',
+    //     onAction: onRefresh
+    //   }}
+    //   image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+    // >
+    //   <p>Switch to the search tab to accept some HITs.</p>
+    // </EmptyState>
+    <NonIdealState
+      title="Your queue is empty"
+      description="You can click refresh or switch to the search tab."
+      visual="refresh"
+      action={
+        <Button primary onClick={onRefresh}>
+          Refresh
+        </Button>
+      }
+    />
   );
 };
 
