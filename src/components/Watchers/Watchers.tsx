55ed878e69cda83ec7493d1ce2556d4a10ee50fd
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-16 18:27:23 -0400

    Delete comments.

--- src/components/Watchers/Watchers.tsx
+++ src/components/Watchers/Watchers.tsx
@@ -39,7 +39,6 @@ class Watchers extends React.PureComponent<Props, never> {
         >
           <WatcherInput />
         </Layout.AnnotatedSection>
-
         <Layout.Section secondary>
           {Watchers.generateColumn(0)(watcherIds)}
         </Layout.Section>
@@ -52,40 +51,6 @@ class Watchers extends React.PureComponent<Props, never> {
       </Layout>
     );
   }
-  // public render() {
-  //   return (
-  //     <div className="pt-tree pt-elevation-0">
-  //       <ul className="pt-tree-node-list pt-tree-root">
-  //         <li className="pt-tree-node pt-tree-node-expanded">
-  //           <div className="pt-tree-node-content">
-  //             <span className="pt-tree-node-caret pt-tree-node-caret-open pt-icon-standard" />
-  //             <span className="pt-tree-node-icon pt-icon-standard pt-icon-folder-close" />
-  //             <span className="pt-tree-node-label">Label</span>
-  //             <span className="pt-tree-node-secondary-label">
-  //               Secondary label
-  //             </span>
-  //           </div>
-  //           <ul className="pt-tree-node-list">
-  //             <li className="pt-tree-node">
-  //               <div className="pt-tree-node-content">
-  //                 <span className="pt-tree-node-caret-none pt-icon-standard" />
-  //                 <span className="pt-tree-node-icon pt-icon-standard pt-icon-document" />
-  //                 <span className="pt-tree-node-label">A Document</span>
-  //               </div>
-  //             </li>
-  //             <li className="pt-tree-node">
-  //               <div className="pt-tree-node-content">
-  //                 <span className="pt-tree-node-caret-none pt-icon-standard" />
-  //                 <span className="pt-tree-node-icon pt-icon-standard pt-icon-document" />
-  //                 <span className="pt-tree-node-label">Another Document</span>
-  //               </div>
-  //             </li>
-  //           </ul>
-  //         </li>
-  //       </ul>
-  //     </div>
-  //   );
-  // }
 }
 
 export default Watchers;
