996df0308d4024c50057c0cbbe936a18ee302844
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-19 12:30:07 -0400

    Add actions for marking hits as read.

---
+++ src/actions/markAsRead.ts
@@ -0,0 +1,21 @@
+import { MARK_ALL_HITS_AS_READ, MARK_HIT_AS_READ } from '../constants';
+
+export interface MarkHitAsRead {
+  type: MARK_HIT_AS_READ;
+  groupId: string;
+}
+
+export interface MarkAllHitsAsRead {
+  type: MARK_ALL_HITS_AS_READ;
+}
+
+export type MarkAction = MarkHitAsRead | MarkAllHitsAsRead;
+
+export const markHitAsRead = (groupId: string): MarkHitAsRead => ({
+  type: MARK_HIT_AS_READ,
+  groupId
+});
+
+export const markAllHitsAsRead = (): MarkAllHitsAsRead => ({
+  type: MARK_ALL_HITS_AS_READ
+});
