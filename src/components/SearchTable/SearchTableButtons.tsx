8aa72a9fc0fb8fbec2d084ce8f14f16c16ecb10b
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-19 12:57:10 -0400

    SearchTableButtons now successfully marks all hits as read.

--- src/components/SearchTable/SearchTableButtons.tsx
+++ src/components/SearchTable/SearchTableButtons.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
+import { connect, Dispatch } from 'react-redux';
+import { MarkAllHitsAsRead, markAllHitsAsRead } from '../../actions/markAsRead';
 import { Stack, ButtonGroup, Button } from '@shopify/polaris';
 
-export interface Props {}
+export interface Handlers {
+  readonly onMarkAllAsRead: () => void;
+}
 
-class CollapsibleInfo extends React.PureComponent<{}, never> {
+class SearchTableButtons extends React.PureComponent<Handlers, never> {
   public render() {
     return (
       <Stack vertical={false} alignment="center">
         <ButtonGroup>
-          <Button plain>Mark all as read</Button>
+          <Button plain onClick={this.props.onMarkAllAsRead}>
+            Mark all as read
+          </Button>
           <Button plain>Collapse all</Button>
         </ButtonGroup>
       </Stack>
@@ -16,4 +22,8 @@ class CollapsibleInfo extends React.PureComponent<{}, never> {
   }
 }
 
-export default CollapsibleInfo;
+const mapDispatch = (dispatch: Dispatch<MarkAllHitsAsRead>): Handlers => ({
+  onMarkAllAsRead: () => dispatch(markAllHitsAsRead())
+});
+
+export default connect(null, mapDispatch)(SearchTableButtons);
