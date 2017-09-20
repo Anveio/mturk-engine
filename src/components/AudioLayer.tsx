98746d2433787e6edccde6e5b626753d5b971033
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-20 18:09:15 -0400

    AudioLayer is now aware of when new unread hits have been found.

--- src/components/AudioLayer.tsx
+++ src/components/AudioLayer.tsx
@@ -1,19 +1,30 @@
 import * as React from 'react';
-
-import { connect } from 'react-redux';
+import { connect, Dispatch } from 'react-redux';
 import { RootState } from '../types';
+import { newResults } from '../selectors/searchTable';
 
 export interface Props {
   readonly unreadSearchResultSrc: string;
   readonly watcherAcceptSrc: string;
+  readonly numUnreadSearchResults: number;
 }
 
-class AudioLayer extends React.PureComponent<Props, never> {
+export interface Handlers {
+  readonly onNewSearchResult: () => void;
+}
+
+class AudioLayer extends React.PureComponent<Props & Handlers, never> {
+  componentWillReceiveProps(props: Props) {
+    if (props.numUnreadSearchResults > this.props.numUnreadSearchResults) {
+      this.props.onNewSearchResult();
+    }
+  }
+
   public render() {
     return (
       <div id="audio-layer">
         <audio id="new-search-result-audio">
-          <source src={this.props.unreadSearchResultSrc} type="audio/ogg"/>
+          <source src={this.props.unreadSearchResultSrc} type="audio/ogg" />
         </audio>
         <audio id="watcher-accept-audio">
           <source src={this.props.watcherAcceptSrc} type="audio/ogg" />
@@ -25,7 +36,12 @@ class AudioLayer extends React.PureComponent<Props, never> {
 
 const mapState = (state: RootState): Props => ({
   unreadSearchResultSrc: state.audioSettings.unreadSearchResult,
-  watcherAcceptSrc: state.audioSettings.watcherAccept
+  watcherAcceptSrc: state.audioSettings.watcherAccept,
+  numUnreadSearchResults: newResults(state).size
 });
 
-export default connect(mapState)(AudioLayer);
+const mapDispatch = (dispatch: Dispatch<any>): Handlers => ({
+  onNewSearchResult: () => console.log('new hit found')
+});
+
+export default connect(mapState, mapDispatch)(AudioLayer);
