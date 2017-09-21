8ca13181ed1858c3deb7652e39020de467b3d6f9
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-20 20:23:00 -0400

    Edit copy.

--- src/components/Watchers/EmptyWatchers.tsx
+++ src/components/Watchers/EmptyWatchers.tsx
@@ -9,7 +9,9 @@ class EmptyWatchers extends React.PureComponent<Props, never> {
       <NonIdealState
         title="You have no watchers."
         description={`Watchers will periodically accept a HIT. 
-        They're useful for hoarding lots of HITs or snagging rare ones.`}
+        They're useful for hoarding lots of HITs or snagging rare ones.
+        You can add them directly from search results or manually with a groupID
+        or a pandA link`}
         visual="pt-icon-folder-shared-open"
       />
     );
