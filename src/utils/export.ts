cf94bfc0566e547e5a5b68c31ad0a6c203d32bb4
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-25 15:53:25 -0400

    Fix contact link in Markdown export.

--- src/utils/export.ts
+++ src/utils/export.ts
@@ -26,8 +26,10 @@ export const generateMarkdownExport = (hit: SearchResult): string => {
     qualsRequired
   } = hit;
 
-  const quals = qualsRequired.length === 0 ? 'None' :
-  qualsRequired.map(removExtraneousWhiteSpace).join('; ');
+  const quals =
+    qualsRequired.length === 0
+      ? 'None'
+      : qualsRequired.map(removExtraneousWhiteSpace).join('; ');
 
   if (requester.turkopticon) {
     const {
@@ -39,18 +41,17 @@ export const generateMarkdownExport = (hit: SearchResult): string => {
     // tslint:disable:max-line-length
     return `> **Title:** [${title}](${acceptBaseUrl}${groupId})  
     **Worker:** [Preview](${baseTaskUrlWorker}${groupId}/tasks) | [Accept](${baseTaskUrlWorker}${groupId}/tasks/accept_random) | [Requester](${baseRequeserUrlWorker}${requester.id}/projects)  
-    **Requester:** [Xtractions](${requesterSearchBaseUrl}${requester.id}) [${requester.id}] ([Contact](${contactBaseUrl}${requester.id}))  
+    **Requester:** [${requester.name}](${requesterSearchBaseUrl}${requester.id}) ([Contact](${contactBaseUrl}${requester.id}))  
     **[TO](${turkopticonBaseUrl}${requester.id}):** [Pay: ${pay}] [Fast: ${fast}] [Comm: ${comm}] [Fair: ${fair}] [Reviews: ${reviews}] [ToS: ${tos_flags}]  
     **Reward:** $${reward} 
     **Duration:** ${timeAllotted}  
     **Available:** ${batchSize}  
     **Description:** ${description}
     **Requirements:** ${quals}`;
-    
   } else {
     return `> **Title:** [${title}](${acceptBaseUrl}${groupId})  
     **Worker:** [Preview](${baseTaskUrlWorker}${groupId}/tasks) | [Accept](${baseTaskUrlWorker}${groupId}/tasks/accept_random) | [Requester](${baseRequeserUrlWorker}${requester.id}/projects)  
-    **Requester:** [Xtractions](${requesterSearchBaseUrl}${requester.id}) [${requester.id}] ([Contact](${contactBaseUrl}${requester.id}))  
+    **Requester:** [${requester.name}](${requesterSearchBaseUrl}${requester.id}) [${requester.id}] ([Contact](${contactBaseUrl}${requester.id}))  
     **[TO](No data)]  
     **Reward:** $${reward} 
     **Duration:** ${timeAllotted}  
