e1e2a94a54b7b9639f79053ae0890573cab5b3b1
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-26 11:46:28 -0400

    Add viewport meta tag.

--- src/components/CustomHead.tsx
+++ src/components/CustomHead.tsx
@@ -23,6 +23,8 @@ class CustomHead extends React.PureComponent<Props, never> {
           this.props.queueSize
         )}
       >
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <style>{toastrStylesheet}</style>
         <style>{polarisStylesheet}</style>
         <link
