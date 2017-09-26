c46b4bd30f684dcb6428e07bb29dad9251880dc5
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-26 18:15:18 -0400

    DisconnectedAccount component is now connected and dispatches CONNECT_ACCOUNT_REQUEST.

---
+++ src/components/Account/DisconnectedAccount.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { connect, Dispatch } from 'react-redux';
+import { Layout, AccountConnection } from '@shopify/polaris';
+import {
+  connectAccountRequest,
+  ConnectAccountRequest
+} from '../../actions/connectAccount';
+
+export interface Handlers {
+  readonly onConnect: () => void;
+}
+
+class DisconnectedAccount extends React.PureComponent<Handlers, never> {
+  public render() {
+    return (
+      <Layout.AnnotatedSection
+        title="Account"
+        description="Connect your account to your Shopify store."
+      >
+        <AccountConnection
+          action={{
+            content: 'Connect',
+            onAction: this.props.onConnect
+          }}
+          details="No account connected"
+          termsOfService={
+            <p>
+              By clicking Connect, you are accepting Sample’s terms and
+              conditions.
+            </p>
+          }
+        />
+      </Layout.AnnotatedSection>
+    );
+  }
+}
+
+const mapDispatch = (dispatch: Dispatch<ConnectAccountRequest>): Handlers => ({
+  onConnect: () => dispatch(connectAccountRequest())
+});
+
+export default connect(null, mapDispatch)(DisconnectedAccount);
