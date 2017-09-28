873ea15071315eb6dcb0305d14f52f125ccb5b0b
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-27 23:36:07 -0400

    SearchTableHeading now shows the number of new results.

--- src/components/SearchTable/SearchTableHeading.tsx
+++ src/components/SearchTable/SearchTableHeading.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { RootState } from '../../types';
 import { Card, Heading } from '@shopify/polaris';
-import { resultsLengthSelector } from '../../selectors/searchTable';
+import { resultsLengthSelector, newResults } from '../../selectors/searchTable';
 
 export interface Props {
   readonly rawResultsSize: number;
+  readonly numNewHits: number;
 }
 
 export interface OwnProps {
@@ -13,14 +14,18 @@ export interface OwnProps {
 }
 
 class SearchTableHeading extends React.PureComponent<Props & OwnProps, never> {
+  static showNewResults = (numNew: number) =>
+    numNew > 0 ? `${numNew} new, ` : '';
+
   public render() {
-    const { displayedResultsSize, rawResultsSize } = this.props;
+    const { displayedResultsSize, numNewHits, rawResultsSize } = this.props;
     const numHiddenResults = rawResultsSize - displayedResultsSize;
 
     return (
       <Card.Section>
         <Heading>
-          Showing {displayedResultsSize} results. ({numHiddenResults} hidden)
+          Showing {displayedResultsSize} results. ({SearchTableHeading.showNewResults(numNewHits)}
+          {numHiddenResults} hidden)
         </Heading>
       </Card.Section>
     );
@@ -28,7 +33,8 @@ class SearchTableHeading extends React.PureComponent<Props & OwnProps, never> {
 }
 
 const mapState = (state: RootState): Props => ({
-  rawResultsSize: resultsLengthSelector(state)
+  rawResultsSize: resultsLengthSelector(state),
+  numNewHits: newResults(state).size
 });
 
 export default connect(mapState)(SearchTableHeading);
