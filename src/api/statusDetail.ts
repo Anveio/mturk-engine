f2942f0b083a50e47c3980fc12312dfc020da3db
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-29 19:39:20 -0400

    EncodedDateString is now sent as a param instead of through string interpolation.

--- src/api/statusDetail.ts
+++ src/api/statusDetail.ts
@@ -2,14 +2,18 @@ import axios from 'axios';
 import { API_URL } from '../constants';
 import { parseStatusDetailPage } from '../utils/parsingStatusDetail';
 
+/**
+ * E.g. '09222017'
+ * @param encodedDateString 
+ */
 export const fetchStatusDetailPage = async (encodedDateString: string) => {
   try {
-    const response = await axios.get(
-      `${API_URL}/statusdetail?encodedDate=${encodedDateString}`,
-      {
-        responseType: 'document'
-      }
-    );
+    const response = await axios.get(`${API_URL}/statusdetail`, {
+      params: {
+        encodedDate: encodedDateString
+      },
+      responseType: 'document'
+    });
     return parseStatusDetailPage(response.data as Document);
   } catch (e) {
     throw Error('Problem fetching status detail: ' + e);
