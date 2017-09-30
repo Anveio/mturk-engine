0e5ec26bfae18e95b3b2051392ab2cdabff5ed46
Shovon Hasan <im.shovonhasan@gmail.com>
2017-09-29 22:22:18 -0400

    handleStatusDetailRequest now recursively calls itself if more pages are detected.

--- src/sagas/statusDetail.ts
+++ src/sagas/statusDetail.ts
@@ -3,10 +3,14 @@ import {
   FetchStatusDetailFailure,
   FetchStatusDetailRequest,
   FetchStatusDetailSuccess,
+  statusDetailRequest,
   statusDetailFailure,
   statusDetailSuccess
 } from '../actions/statusDetail';
-import { fetchStatusDetailPage } from '../api/statusDetail';
+import {
+  fetchStatusDetailPage,
+  StatusDetailPageInfo
+} from '../api/statusDetail';
 
 export function* handleStatusDetailRequest(action: FetchStatusDetailRequest) {
   try {
@@ -15,9 +19,28 @@ export function* handleStatusDetailRequest(action: FetchStatusDetailRequest) {
      */
     const encodedDateString = '09222017';
 
-    const hitDBMap = yield call(fetchStatusDetailPage, encodedDateString);
-    yield put<FetchStatusDetailSuccess>(statusDetailSuccess(hitDBMap));
+    const pageInfo: StatusDetailPageInfo = yield call(
+      fetchStatusDetailPage,
+      encodedDateString
+    );
+    const { data, morePages } = pageInfo;
+
+    if (data.isEmpty()) {
+      yield put<FetchStatusDetailFailure>(statusDetailFailure());
+    } else {
+      yield put<FetchStatusDetailSuccess>(statusDetailSuccess(data));
+    }
+
+    /**
+     * Recursively call this function with page+1.
+     */
+    if (morePages) {
+      yield put<FetchStatusDetailRequest>(
+        statusDetailRequest(action.date, action.page + 1)
+      );
+    }
   } catch (e) {
+    console.warn(e);
     yield put<FetchStatusDetailFailure>(statusDetailFailure());
   }
 }
