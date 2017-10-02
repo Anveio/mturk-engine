52aa60a3318ffda429cfb7df780afece0c4dbdc2
Shovon Hasan <im.shovonhasan@gmail.com>
2017-10-02 09:17:59 -0400

    Remove unneeded State.

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -4,35 +4,33 @@ import { Card } from '@shopify/polaris';
 import { connect } from 'react-redux';
 import { oneYearOfData } from '../selectors/hitDatabase';
 import CalendarHeatmap from './CustomCalendar';
-import { List } from 'immutable'
+import { List } from 'immutable';
 
 interface Props {
   readonly values: List<HeatMapValue>;
+  readonly selectedDateData: {};
 }
 
-interface State {
-  readonly hoveredElem?: SVGRectElement;
-}
-
-class Calendar extends React.PureComponent<Props, State> {
-  state: State = { hoveredElem: undefined };
-
+class Calendar extends React.PureComponent<Props, never> {
   public render() {
     return (
-      <Card sectioned>
-        <CalendarHeatmap
-          endDate={new Date()}
-          numDays={365}
-          values={this.props.values}
-          gutterSize={1}
-        />
+      <Card>
+        <Card.Section title="HIT Database">
+          <CalendarHeatmap
+            endDate={new Date()}
+            numDays={365}
+            values={this.props.values}
+            gutterSize={1}
+          />
+        </Card.Section>
       </Card>
     );
   }
 }
 
 const mapState = (state: RootState): Props => ({
-  values: oneYearOfData(state)
+  values: oneYearOfData(state),
+  selectedDateData: 
 });
 
 export default connect(mapState)(Calendar);
