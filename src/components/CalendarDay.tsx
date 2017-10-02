a73d433e5b0925d1418d39e0d7b92f44891b069d
Shovon Hasan <im.shovonhasan@gmail.com>
2017-10-02 01:48:48 -0400

    CalendarDay no longer accepts null values.

--- src/components/CalendarDay.tsx
+++ src/components/CalendarDay.tsx
@@ -2,13 +2,22 @@ import * as React from 'react';
 import { HeatMapValue } from '../types';
 
 export interface Props {
-  x: number;
-  y: number;
-  value: HeatMapValue | null;
-  squareSize: number;
+  readonly x: number;
+  readonly y: number;
+  readonly value: HeatMapValue;
+  readonly squareSize: number;
 }
 
-class CalendarDay extends React.PureComponent<Props, never> {
+export interface State {
+  readonly hovering: boolean;
+}
+
+class CalendarDay extends React.PureComponent<Props, State> {
+  readonly state: State = { hovering: false };
+
+  static calculateTooltipVisibility = (hovering: boolean) =>
+    hovering ? 'visible' : 'hidden';
+
   static calculateGrade = (count: number): string => {
     if (count >= 20) {
       return 'color-github-4';
@@ -21,18 +30,35 @@ class CalendarDay extends React.PureComponent<Props, never> {
     }
   };
 
-  static generateClassName = (value: HeatMapValue | null): string => {
-    if (value) {
+  static generateClassName = (value: HeatMapValue): string => {
+    if (value.count > 0) {
       return 'color-filled ' + CalendarDay.calculateGrade(value.count);
     } else {
       return 'color-empty';
     }
   };
 
+  static titleForValue = (value: HeatMapValue) => {
+    // return value !== null
+    //   ? `$${value.count} earned on ${value.date.toLocaleDateString()}`
+    //   : null;
+  };
+
+  private handleMouseEnter = () => {
+    this.setState({ hovering: true });
+  };
+
+  private handleMouseLeave = () => {
+    this.setState({ hovering: false });
+  };
+
   public render() {
     const { x, y, squareSize, value } = this.props;
     return (
-      <g>
+      <g
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+      >
         <rect
           className={CalendarDay.generateClassName(value)}
           width={squareSize}
@@ -40,7 +66,15 @@ class CalendarDay extends React.PureComponent<Props, never> {
           x={x}
           y={y}
           onClick={() => console.log(value)}
-        />
+        >
+          <text
+            style={{
+              visibility: CalendarDay.calculateTooltipVisibility(
+                this.state.hovering
+              )
+            }}
+          />
+        </rect>
       </g>
     );
   }
