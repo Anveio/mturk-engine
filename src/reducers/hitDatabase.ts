7ca67e15621d521d97750d9fc25f4dbca9abffc8
Shovon Hasan <im.shovonhasan@gmail.com>
2017-10-03 10:06:46 -0400

    Merging STATUS_DETAIL_SUCCESS data will now preserve existing bonuses.

--- src/reducers/hitDatabase.ts
+++ src/reducers/hitDatabase.ts
@@ -2,6 +2,7 @@ import { HitDatabaseEntry, HitDatabaseMap } from '../types';
 import { FetchStatusDetailSuccess } from '../actions/statusDetail';
 import { STATUS_DETAIL_SUCCESS } from '../constants';
 import { Map } from 'immutable';
+import { conflictsPreserveBonus } from '../utils/hitDatabase';
 // import sampleHitDB from '../utils/sampleHitDatabase';
 
 const initial: HitDatabaseMap = Map<string, HitDatabaseEntry>();
@@ -12,7 +13,7 @@ export default (
 ): HitDatabaseMap => {
   switch (action.type) {
     case STATUS_DETAIL_SUCCESS:
-      return state.merge(action.data);
+      return state.mergeWith(conflictsPreserveBonus, action.data);
     default:
       return state;
   }
---
+++ src/utils/hitDatabase.ts
@@ -0,0 +1,11 @@
+import { HitDatabaseEntry } from '../types';
+
+export const conflictsPreserveBonus = (
+  oldEntry: HitDatabaseEntry,
+  newEntry: HitDatabaseEntry
+): HitDatabaseEntry => {
+  return {
+    ...newEntry,
+    bonus: oldEntry.bonus
+  };
+};
