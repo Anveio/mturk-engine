c9eb50ed879e33625b97c067f73fe14d9537de08
Shovon Hasan <im.shovonhasan@gmail.com>
2017-10-03 11:02:05 -0400

    Edit reducer to respond to EDIT_BONUS.

--- src/reducers/hitDatabase.ts
+++ src/reducers/hitDatabase.ts
@@ -1,6 +1,7 @@
 import { HitDatabaseEntry, HitDatabaseMap } from '../types';
 import { FetchStatusDetailSuccess } from '../actions/statusDetail';
-import { STATUS_DETAIL_SUCCESS } from '../constants';
+import { EditBonus } from '../actions/bonus';
+import { STATUS_DETAIL_SUCCESS, EDIT_BONUS } from '../constants';
 import { Map } from 'immutable';
 import { conflictsPreserveBonus } from '../utils/hitDatabase';
 // import sampleHitDB from '../utils/sampleHitDatabase';
@@ -9,11 +10,16 @@ const initial: HitDatabaseMap = Map<string, HitDatabaseEntry>();
 
 export default (
   state = initial,
-  action: FetchStatusDetailSuccess
+  action: FetchStatusDetailSuccess | EditBonus
 ): HitDatabaseMap => {
   switch (action.type) {
     case STATUS_DETAIL_SUCCESS:
       return state.mergeWith(conflictsPreserveBonus, action.data);
+    case EDIT_BONUS:
+      return state.update(action.id, (hit) => ({
+        ...hit,
+        bonus: action.bonus
+      }));
     default:
       return state;
   }
