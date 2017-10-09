ededc1f9dace8f97f7eb64cab0030a3d6dc9d3df
Shovon Hasan <im.shovonhasan@gmail.com>
2017-10-09 13:18:39 -0400

    Progress bar is no longer animated.

--- src/components/Account/DailyEarningsProgressBar.tsx
+++ src/components/Account/DailyEarningsProgressBar.tsx
@@ -5,36 +5,25 @@ import { ProgressBar } from '@blueprintjs/core';
 import { RootState } from '../../types';
 import { todaysProjectedEarnings } from '../../selectors/hitDatabase';
 
-
-
 export interface Props {
   readonly dailyEarningsGoal: number;
   readonly todaysEarnings: number;
 }
 
-
-
-class DailyEarningsProgressBar extends React.PureComponent<
-  Props,
-  never
-> {
+class DailyEarningsProgressBar extends React.PureComponent<Props, never> {
   static calculateProgress = (value: number, goal: number) => value / goal;
 
   public render() {
     const { dailyEarningsGoal, todaysEarnings } = this.props;
     return (
-      // <Card sectioned title="Daily Earnings Goal">
-      //   <Stack vertical>
-      //     <DisplayText>{formatAsCurrency(dailyEarningsGoal)}</DisplayText>
       <ProgressBar
+        className="pt-no-stripes pt-no-animation"
         value={DailyEarningsProgressBar.calculateProgress(
           todaysEarnings,
           dailyEarningsGoal
         )}
         intent={1}
       />
-      //   </Stack>
-      // </Card>
     );
   }
 }
@@ -43,7 +32,5 @@ const mapState = (state: RootState): Props => ({
   dailyEarningsGoal: state.dailyEarningsGoal,
   todaysEarnings: todaysProjectedEarnings(state)
 });
-
-
 
 export default connect(mapState)(DailyEarningsProgressBar);
