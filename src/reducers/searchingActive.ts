9cd5dfb2f2ff4430ac898f60308b891cc055009d
Shovon Hasan <im.shovonhasan@gmail.com>
2017-10-18 20:49:34 -0400

    Failed or empty searches will not set searchingActive to false.

--- src/reducers/searchingActive.ts
+++ src/reducers/searchingActive.ts
@@ -1,12 +1,18 @@
-import { TOGGLE_SEARCH_ACTIVITY } from '../constants';
+import { TOGGLE_SEARCH_ACTIVITY, SEARCH_FAILURE } from '../constants';
 import { ToggleSearchActive } from '../actions/updateValue';
+import { SearchFailure } from '../actions/search';
 
 const initial = false;
 
-export default (state = initial, action: ToggleSearchActive) => {
+export default (
+  state = initial,
+  action: ToggleSearchActive | SearchFailure
+) => {
   switch (action.type) {
     case TOGGLE_SEARCH_ACTIVITY:
       return !state;
+    case SEARCH_FAILURE:
+      return false;
     default:
       return state;
   }
