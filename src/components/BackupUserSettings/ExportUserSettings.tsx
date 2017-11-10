0e9a7648d677193a75adfee89b79ff01ec667d84
Shovon Hasan <im.shovonhasan@gmail.com>
2017-11-10 13:49:35 -0500

    Add function to stringify persisted state.

--- src/components/BackupUserSettings/ExportUserSettings.tsx
+++ src/components/BackupUserSettings/ExportUserSettings.tsx
@@ -1,8 +1,37 @@
 import * as React from 'react';
+import * as localforage from 'localforage';
 // import { Button } from '@blueprintjs/core';
 export interface Props {}
 
 class ExportUserSettings extends React.PureComponent<Props, never> {
+  static stringifyPersistedState = async () => {
+    try {
+      const localForageKeys: string[] = await localforage.keys();
+      return await Promise.all(
+        localForageKeys.map(
+          async key => `"${key}":` + (await localforage.getItem(key))
+        )
+      );
+    } catch (e) {
+      throw new Error('Failed to read user settings.');
+    }
+  };
+
+  private downloadDataAsFile = async () => {
+    try {
+      const data = await ExportUserSettings.stringifyPersistedState();
+      console.log(data.join(''));
+    } catch (e) {}
+  };
+
+  // static uploadDataFromFile = (stringifedUserSettings: string[]) => {
+  //   const x = stringifedUserSettings.reduce((acc, cur: string) => {
+  //     const [key, value] = cur.split(/"reduxPersist:(.*?)"/).slice(1);
+  //     return { ...acc, [key]: value };
+  //   }, {});
+  //   console.log(x);
+  // };
+
   public render() {
     return (
       <div className="pt-button-group .modifier">
@@ -12,8 +41,13 @@ class ExportUserSettings extends React.PureComponent<Props, never> {
         <a className="pt-button pt-icon-function" role="button">
           Functions
         </a>
-        <a className="pt-button" role="button">
-          Options{' '}
+        <a
+          className="pt-button"
+          role="button"
+          onClick={this.downloadDataAsFile}
+          download=""
+        >
+          Export{' '}
           <span className="pt-icon-standard pt-icon-caret-down pt-align-right" />
         </a>
       </div>
