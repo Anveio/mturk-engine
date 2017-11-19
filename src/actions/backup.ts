52560fbef7840e7e572dbbe89a3b58ae532855f9
Shovon Hasan <im.shovonhasan@gmail.com>
2017-11-19 13:51:44 -0500

    Create action for writing to persisted state.

--- src/actions/backup.ts
+++ src/actions/backup.ts
@@ -1,9 +1,22 @@
-import { READ_PERSISTED_STATE } from '../constants';
+import { READ_PERSISTED_STATE, WRITE_PERSISTED_STATE } from '../constants';
+import { PersistedState } from '../types';
 
 export interface ReadPersistedState {
   readonly type: READ_PERSISTED_STATE;
 }
 
+export interface WritePersistedState {
+  readonly type: WRITE_PERSISTED_STATE;
+  readonly payload: Partial<PersistedState>;
+}
+
 export const readPersistedState = (): ReadPersistedState => ({
   type: READ_PERSISTED_STATE
+});
+
+export const writePersistedState = (
+  payload: Partial<PersistedState>
+): WritePersistedState => ({
+  type: WRITE_PERSISTED_STATE,
+  payload
 });
