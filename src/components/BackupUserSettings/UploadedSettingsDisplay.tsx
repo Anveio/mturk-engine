91dd66481b97cae83b98a6953cc4f25773680e88
Shovon Hasan <im.shovonhasan@gmail.com>
2017-11-20 07:37:16 -0500

    UploadedSettingsDisplay now shows a checkbox.

--- src/components/BackupUserSettings/UploadedSettingsDisplay.tsx
+++ src/components/BackupUserSettings/UploadedSettingsDisplay.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { connect, Dispatch } from 'react-redux';
 import { Card, FormLayout } from '@shopify/polaris';
+import { Checkbox } from '@blueprintjs/core';
 import { RootState, PersistedState } from '../../types';
 import { writePersistedState, WritePersistedState } from '../../actions/backup';
 import {
   validatePersistedStateKey,
   selectReduxPersistStateKey
 } from '../../utils/validation';
+import { stateKeyMap } from '../../utils/backup';
 
 export interface Props {
   readonly uploadedState: Partial<PersistedState> | null;
@@ -22,8 +24,10 @@ class UploadedSettingsDisplay extends React.Component<Props & Handlers, never> {
       .filter(validatePersistedStateKey)
       .map(selectReduxPersistStateKey);
 
-    return stateKeys.length === 0 ? (
-      stateKeys.map(key => <div key={key}>{key}</div>)
+    return stateKeys.length > 0 ? (
+      stateKeys.map(stateKey => (
+        <Checkbox key={stateKey} label={stateKeyMap[stateKey]} checked />
+      ))
     ) : (
       <p>The uploaded file contains no valid data.</p>
     );
