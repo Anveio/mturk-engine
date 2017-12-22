fd58c4eff62d524c053b20b27f7644696d99976b
Shovon Hasan <im.shovonhasan@gmail.com>
2017-12-22 10:17:41 -0500

    Delete unused toast.

--- src/components/Toasts.tsx
+++ src/components/Toasts.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import { Stack, Spinner, TextContainer } from '@shopify/polaris';
-import { connect, Dispatch } from 'react-redux';
-import { AddWatcher, addWatcher } from '../actions/watcher';
-import { watcherFromId } from '../utils/watchers';
 
 interface GenericProps {
   readonly message: string;
@@ -35,30 +32,3 @@ export const UploadProgressToast = ({ progress }: UploadProps) => {
     'File upload complete'
   );
 };
-
-interface Handlers {
-  readonly onAddWatcher: (groupId: string) => void;
-}
-
-interface Props {
-  readonly groupId: string;
-}
-
-const mapDispatch = (dispatch: Dispatch<AddWatcher>): Handlers => ({
-  onAddWatcher: (groupId: string) =>
-    dispatch(addWatcher(watcherFromId(groupId)))
-});
-
-class AddWatcherToast extends React.PureComponent<Props & Handlers, never> {
-  public render() {
-    return (
-      <Stack>
-        <TextContainer>Couldn't add that HIT to your queue.</TextContainer>
-      </Stack>
-    );
-  }
-}
-
-export const AddUnacceptedHitAsWatcher = connect(null, mapDispatch)(
-  AddWatcherToast
-);
