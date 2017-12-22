5f33d97c64bead696b0c5bb24aded772845e5741
Shovon Hasan <im.shovonhasan@gmail.com>
2017-12-22 05:16:13 -0500

    Fetching hit submission history now uses the JSON api.

--- src/api/dashboard.ts
+++ src/api/dashboard.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import { API_URL } from '../constants';
 import { generateAccountInfo } from '../utils/parsingWorkerAccount';
-import { parseWorkerHitHistory } from '../utils/parsingWorkerHitHistory';
+import { parseWorkedDates } from '../utils/parsingWorkerHitHistory';
+import { DashboardApiResponse } from '../worker-mturk-api';
 
 export const fetchDashboard = async () => {
   try {
     const response = await axios.get<Document>(`${API_URL}/dashboard`, {
       responseType: 'document'
     });
+
     return generateAccountInfo(response.data);
   } catch (e) {
     throw Error('Problem fetching dashboard. ' + e);
@@ -16,10 +18,16 @@ export const fetchDashboard = async () => {
 
 export const fetchSubmittedHitHistory = async () => {
   try {
-    const response = await axios.get<Document>(`${API_URL}/dashboard`, {
-      responseType: 'document'
-    });
-    return parseWorkerHitHistory(response.data);
+    const response = await axios.get<DashboardApiResponse>(
+      `${API_URL}/dashboard`,
+      {
+        params: {
+          format: 'json'
+        },
+        responseType: 'json'
+      }
+    );
+    return parseWorkedDates(response.data);
   } catch (e) {
     throw Error('Problem fetching status summary: ' + e);
   }
--- src/utils/parsingWorkerHitHistory.ts
+++ src/utils/parsingWorkerHitHistory.ts
@@ -1,29 +1,12 @@
-import { workerHitHistoryDateAnchor } from '../constants/querySelectors';
+import { DashboardApiResponse } from '../worker-mturk-api';
 
 /**
  * Returns an array of Mturk's URL encoded date strings for each day that has
  * HIT data.
  * @param html
  */
-export const parseWorkerHitHistory = (html: Document): string[] => {
-  const dateCells = html.querySelectorAll(workerHitHistoryDateAnchor);
-  const unfilteredDates = Array.from(dateCells).map((date: Element) =>
-    parseEncodedDateString(date)
-  );
-
-  return unfilteredDates.filter(el => el !== '');
-};
-
-/**
- * Returns a date string in the format of YYYY-MM-DD
- * @param input
- */
-const parseEncodedDateString = (input: Element): string => {
-  const href = input.getAttribute('href');
-  if (href && href.split('/status_details/').length > 1) {
-    return href.split('/status_details/')[1];
-  } else {
-    console.warn(`Error parsing date string for ${input}, href: ${href}`);
-    return '';
-  }
+export const parseWorkedDates = (data: DashboardApiResponse): Date[] => {
+  return data.daily_hit_statistics_overview
+    .filter(overview => overview.submitted > 0)
+    .map(overview => new Date(overview.date));
 };
