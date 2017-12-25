528ca7036b6d4acef2eaa8ae159c41bd6628f428
Shovon Hasan <im.shovonhasan@gmail.com>
2017-12-25 10:41:25 -0500

    Add warning banner.

--- src/components/Queue/QueueTable.tsx
+++ src/components/Queue/QueueTable.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { connect, Dispatch } from 'react-redux';
-import { Layout, Card, ResourceList, Stack, Button } from '@shopify/polaris';
+import {
+  Layout,
+  Card,
+  ResourceList,
+  Stack,
+  Banner,
+  Button
+} from '@shopify/polaris';
 import { RootState } from '../../types';
 import { queueItemsIds } from '../../selectors/queue';
 import { FetchQueueRequest, fetchQueueRequest } from '../../actions/queue';
@@ -17,7 +24,13 @@ export interface Handlers {
   readonly onRefresh: () => void;
 }
 
-class QueueTable extends React.PureComponent<Props & Handlers, never> {
+interface State {
+  readonly bannerVisible: boolean;
+}
+
+class QueueTable extends React.PureComponent<Props & Handlers, State> {
+  public readonly state: State = { bannerVisible: true };
+
   componentWillReceiveProps({ selectedTabIndex }: Props & Handlers) {
     if (
       this.props.selectedTabIndex !== selectedTabIndex &&
@@ -26,6 +39,8 @@ class QueueTable extends React.PureComponent<Props & Handlers, never> {
       this.props.onRefresh();
     }
   }
+
+  private dismissBanner = () => this.setState({ bannerVisible: false });
 
   public render() {
     const { queueItemIds, onRefresh } = this.props;
@@ -36,6 +51,14 @@ class QueueTable extends React.PureComponent<Props & Handlers, never> {
       <Layout>
         <Layout.Section>
           <Stack vertical>
+            {this.state.bannerVisible ? (
+              <Banner status="warning" onDismiss={this.dismissBanner}>
+                Mturk Engine incorrectly reports a successful accept when
+                encountering a CAPTCHA. If you refresh your queue and your HIT
+                isn't there, accept it manually and successfully complete the
+                CAPTCHA.
+              </Banner>
+            ) : null}
             <Card sectioned>
               <Button onClick={onRefresh}>Refresh queue.</Button>
             </Card>
