d01411e0e409ef361bddb85ee5535ce05ab2b63a
Shovon Hasan <im.shovonhasan@gmail.com>
2017-12-28 17:31:15 -0500

    Add first tests for calculateWeightedAverageScore.

--- src/__test__/requesterAverageScore.test.ts
+++ src/__test__/requesterAverageScore.test.ts
@@ -8,11 +8,32 @@ const equalizedWeights: AttributeWeights = {
   payWeight: 1
 };
 
-const noNullAttributeScores: RequesterAttributes = {
-  comm: 1,
-  fair: 1,
-  fast: 1,
-  pay: 1
+const allZeroWeights: AttributeWeights = {
+  commWeight: 0,
+  fairWeight: 0,
+  fastWeight: 0,
+  payWeight: 0
 };
 
+const noNullAttributeScores: RequesterAttributes = {
+  comm: 5,
+  fair: 5,
+  fast: 5,
+  pay: 5
+};
 
+const someNullAttributeScores = {};
+
+describe('Calculating requester average T.O. score', () => {
+  test('equalized weights and no null attributes', () => {
+    expect(
+      calculateWeightedAverageScore(noNullAttributeScores, equalizedWeights)
+    ).toEqual(5);
+  });
+
+  test('all zero weights and no null attributes', () => {
+    expect(
+      calculateWeightedAverageScore(noNullAttributeScores, allZeroWeights)
+    ).toEqual(null);
+  });
+});
