1f784f27b316cc346b86385eefd113f19e8c6a16
Shovon Hasan <im.shovonhasan@gmail.com>
2018-01-19 15:21:32 -0500

    Fixed lifetime rejected HITs always being 0.

--- src/constants/querySelectors.ts
+++ src/constants/querySelectors.ts
@@ -19,6 +19,6 @@ export const lifetimeBonusEarningsQuerySelector = `#dashboard-earnings-to-date >
 export const lifetimeHitEarningsQuerySelector = `#dashboard-earnings-to-date > div > table > tbody > tr:nth-child(1) > td.text-xs-right`;
 export const lifetimeTotalEarningsQuerySelector = `#dashboard-earnings-to-date > div > table > tbody > tr:nth-child(3) > td.text-xs-right`;
 export const lifetimeApprovedQuerySelector = `#dashboard-hits-overview > div > div > div:nth-child(1) > div.col-xs-5.text-xs-right`;
-export const lifetimeRejectedQuerySelector = `#HitsOverviewAdditionalInfo > div:nth-child(2) > div.col-xs-5.text-xs-right`;
+export const lifetimeRejectedQuerySelector = `#dashboard-hits-overview > div > div > div:nth-child(6) > div.col-xs-5.text-xs-right`;
 export const numPendingQuerySelector = `#dashboard-hits-overview > div > div > div:nth-child(4) > div.col-xs-5.text-xs-right`;
 export const workerAmazonIdReactPropsQuerySelector = `body > div.container-fluid.me-bar > div > div.col-xs-7 > span.text-uppercase > span`;
