6d9d4f10341719e9d3756979841d08a8f0b7dfa0
Shovon Hasan <im.shovonhasan@gmail.com>
2018-01-22 10:45:57 -0500

    Add notification layer.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,6 +4,7 @@ import ErrorBoundary from './ErrorBoundary';
 import TabNavigation from './TabNavigation/TabNavigation';
 import CustomHead from './CustomHead';
 import AudioLayer from './AudioLayer';
+import NotificationLayer from './NotificationLayer';
 import Footer from './Footer';
 
 const App: React.SFC<{}> = () => {
@@ -16,6 +17,7 @@ const App: React.SFC<{}> = () => {
           <Footer />
         </Page>
         <AudioLayer />
+        <NotificationLayer />
       </ErrorBoundary>
     </main>
   );
---
+++ src/components/NotificationLayer.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import { connect, Dispatch } from 'react-redux';
+import { List } from 'immutable';
+import {
+  RootState,
+  SearchResult,
+  SearchResults,
+  NotificationSettings
+} from '../types';
+import { newResultsGroupIdsList, newResults } from '../selectors/search';
+import { sendNotification, SendNotification } from '../actions/notifications';
+
+interface Props {
+  readonly notificationSettings: NotificationSettings;
+  readonly unreadResults: List<string>;
+  readonly newResults: SearchResults;
+}
+
+interface Handlers {
+  readonly onUnreadHit: (hit: SearchResult) => void;
+}
+
+class NotificationLayer extends React.Component<Props & Handlers, never> {
+  componentWillReceiveProps(nextProps: Props) {
+    const { notificationSettings } = this.props;
+    if (
+      !nextProps.unreadResults.isSubset(this.props.unreadResults) &&
+      notificationSettings.enabled
+    ) {
+      const resultsToNotify = NotificationLayer.generateResultsToNotifyUserOf(
+        nextProps.newResults,
+        notificationSettings.minReward
+      );
+      resultsToNotify.forEach((result: SearchResult) =>
+        this.props.onUnreadHit(result)
+      );
+    }
+  }
+
+  private static generateResultsToNotifyUserOf = (
+    results: SearchResults,
+    minReward: number
+  ) =>
+    /**
+     * Users can receive 3 desktop notifications at a time. 
+     * So send notifications for only the 3 highest paying HITs.
+     */
+
+    results
+      .filter((result: SearchResult) => result.reward >= minReward)
+      .sort((a, b) => a.reward - b.reward)
+      .slice(0, 3) as SearchResults;
+
+  public render() {
+    return <> </>;
+  }
+}
+
+const mapState = (state: RootState): Props => ({
+  unreadResults: newResultsGroupIdsList(state),
+  newResults: newResults(state),
+  notificationSettings: state.notificationSettings
+});
+
+const mapDispatch = (dispatch: Dispatch<SendNotification>): Handlers => ({
+  onUnreadHit: (hit: SearchResult) => {
+    dispatch(sendNotification(hit));
+  }
+});
+
+export default connect(mapState, mapDispatch)(NotificationLayer);
