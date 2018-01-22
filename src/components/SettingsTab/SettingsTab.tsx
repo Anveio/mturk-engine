3f82663669d4534dde5069fc0d4264db01dd68d7
Shovon Hasan <im.shovonhasan@gmail.com>
2018-01-22 10:20:38 -0500

    Re-enable Notifications settings section.

--- src/components/SettingsTab/SettingsTab.tsx
+++ src/components/SettingsTab/SettingsTab.tsx
@@ -3,7 +3,7 @@ import { Layout } from '@shopify/polaris';
 import BackupUserSettings from '../BackupUserSettings/BackupUserSettings';
 import AudioSettings from '../AudioSettings/AudioSettings';
 import ToggleNoTO from '../TurkopticonSettings/ToggleNoTO';
-// import NotificationSettings from '../NotificationSettings';
+import NotificationSettings from '../NotificationSettings';
 import MinTOsection from '../TurkopticonSettings/MinTOsection';
 import TurkopticonWeightForm from '../TurkopticonSettings/TurkopticonWeightForm';
 
@@ -12,7 +12,7 @@ const SettingsTab: React.SFC<{}> = () => {
     <Layout>
       <BackupUserSettings />
       <AudioSettings />
-      {/* <NotificationSettings /> */}
+      <NotificationSettings />
       <TurkopticonWeightForm />
       <MinTOsection />
       <ToggleNoTO />
