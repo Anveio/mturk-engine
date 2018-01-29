77c2f85e9a40d407e6d07abc5d8721c6764ce09d
Shovon Hasan <im.shovonhasan@gmail.com>
2018-01-29 16:54:22 -0500

    SubmittedHitCaptionedList now shows a badge for each HIT's payment status.

--- src/components/RequesterWorkHistory/SubmittedHit.tsx
+++ src/components/RequesterWorkHistory/SubmittedHit.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { List, Caption } from '@shopify/polaris';
+import { List, Badge, Caption } from '@shopify/polaris';
 import { formatAsCurrency } from '../../utils/formatting';
 import { stringToDate } from '../../utils/dates';
 import { LEGACY_DATE_FORMAT } from '../../constants/misc';
 import { HitDatabaseEntry } from '../../types';
+import { generateHitStatusBadge } from '../../utils/badges';
 
 interface Props {
   readonly hit: HitDatabaseEntry;
@@ -11,6 +12,10 @@ interface Props {
 const SubmittedHit: React.SFC<Props> = ({ hit }) => (
   <List.Item key={hit.id}>
     {hit.title}
+
+    <Badge status={generateHitStatusBadge(hit.status).status}>
+      {generateHitStatusBadge(hit.status).content}
+    </Badge>
     <Caption>{`${formatAsCurrency(hit.reward)} - Submitted ${stringToDate(
       hit.date
     )(LEGACY_DATE_FORMAT).toLocaleDateString()}`}</Caption>
