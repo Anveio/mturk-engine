0590f6010c514116510b40588113457c55223e10
Shovon Hasan <im.shovonhasan@gmail.com>
2018-02-08 07:20:49 -0500

    Add tests for pluralize function.

---
+++ src/__test__/formatting.test.ts
@@ -0,0 +1,24 @@
+import { pluralize } from '../utils/formatting';
+
+describe('Pluralizing the word "HIT"', () => {
+  const pluralizeHits = pluralize('HIT', 'HITs');
+
+  test('Default argument should always pluralize', () => {
+    expect(pluralizeHits()).toBe('HITs');
+  });
+
+  test('0 should be "HITs"', () => {
+    expect(pluralizeHits(0)).toBe('HITs');
+  });
+
+  test('Singular should be "HIT"', () => {
+    expect(pluralizeHits(1)).toBe('HIT');
+  });
+
+  test('2 or more should be "HITS"', () => {
+    expect(pluralizeHits(2)).toBe('HITs');
+    expect(pluralizeHits(3)).toBe('HITs');
+    expect(pluralizeHits(40)).toBe('HITs');
+    expect(pluralizeHits(500)).toBe('HITs');
+  });
+});
--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -19,21 +19,6 @@ export const formatAsCurrency = currencyFormatter.format;
 export const removeCurrencyFormatting = (input: string) =>
   input.replace(/[^\d.]/g, '');
 
-/**
- * Creates a pluralize function that accepts an amount then returns the
- * appropriate plural form or non-plural form
- * @param nonPluralForm
- * @param pluralForm
- */
 export const pluralize = (nonPluralForm: string, pluralForm: string) => (
   groupSize = 2
 ) => (groupSize === 1 ? nonPluralForm : pluralForm);
-
-expect(pluralize('goose', 'geese')(0)).toBe('geese');
-expect(pluralize('goose', 'geese')());
-
-// Thunked
-const pluralizeGeese = pluralize('goose', 'geese');
-
-expect(pluralizeGeese(0)).toBe('geese');
-expect(pluralizeGeese());
