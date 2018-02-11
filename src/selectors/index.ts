9a3bbb44104c1716f147af559a72de21d3adfdf2
Shovon Hasan <im.shovonhasan@gmail.com>
2018-02-11 09:19:38 -0500

    Add normalizedWatchers selector for backwards compatibility.

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -17,14 +17,6 @@ export const queueSelector = (state: RootState) => state.queue;
 
 export const searchResultSelector = (state: RootState) => state.search;
 
-// export const searchResultSelector = createSelector(
-//   [(state: RootState) => state.search],
-//   results => {
-//     console.log('recomputing search');
-//     return results;
-//   }
-// );
-
 export const selectedHitDbDateSelector = (state: RootState) =>
   state.selectedHitDbDate;
 
@@ -32,9 +24,16 @@ export const sortOptionSelector = (state: RootState) => state.sortingOption;
 
 export const uploadedStateSelector = (state: RootState) => state.uploadedState;
 
+/**
+ * WARNING: Never call this selector directly. Legacy watchers may not have all
+ * the properties of new watchers. Always call normalizedWatchers selectors instead.
+ */
 export const watcherSelector = (state: RootState) => state.watchers;
 
 export const watcherTreeSelector = (state: RootState) => state.watcherTree;
+
+export const watcherFoldersSelector = (state: RootState) =>
+  state.watcherFolders;
 
 export const turkopticonSettingsSelector = (state: RootState) =>
   state.topticonSettings;
--- src/selectors/watchers.ts
+++ src/selectors/watchers.ts
@@ -1,9 +1,28 @@
 import { createSelector } from 'reselect';
 import { Watcher, WatcherMap } from '../types';
 import { watcherSelector } from './index';
+import { Map } from 'immutable';
+import { DEFAULT_WATCHER_FOLDER_ID } from '../constants/misc';
 
-export const watchersSortedLatestFirst = createSelector(
+/**
+ * For backwards compatibility. Legacy watchers on't have certain properties.
+ * Here we ensure they do.
+ */
+export const normalizedWatchers = createSelector(
   [watcherSelector],
+  (watchers: WatcherMap): WatcherMap =>
+    watchers.reduce(
+      (acc: WatcherMap, cur: Watcher) =>
+        acc.set(cur.groupId, {
+          ...cur,
+          folderId: cur.folderId ? cur.folderId : DEFAULT_WATCHER_FOLDER_ID
+        }),
+      Map<string, Watcher>()
+    )
+);
+
+const watchersSortedLatestFirst = createSelector(
+  [normalizedWatchers],
   (watchers: WatcherMap) =>
     watchers.sort((a, b) => b.createdOn.valueOf() - a.createdOn.valueOf())
 );
@@ -17,5 +36,5 @@ export const watcherIdsList = createSelector(
 export const watchersList = createSelector(
   [watcherSelector],
   (watchers: WatcherMap) =>
-    watchers.reduce((acc: Watcher[], cur: Watcher) => acc.concat(cur), [])
+    watchers.reduce((acc: Watcher[], cur: Watcher) => [...acc, cur], [])
 );
