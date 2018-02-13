a45100927ce4f7caf31c37613a479f412b58412a
Shovon Hasan <im.shovonhasan@gmail.com>
2018-02-13 01:22:02 -0500

    Rename validateInteger function.

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -3,7 +3,8 @@ export const validateInbounds = (value: string) => {
   return num >= 0 && num <= 100;
 };
 
-export const validateInteger = (value: string): boolean => /^\d+$/.test(value);
+export const validateIntegerString = (value: string): boolean =>
+  /^\d+$/.test(value);
 
 export const validateNumber = (value: string): boolean =>
   !Number.isNaN(parseFloat(value)) && Number.isFinite(parseFloat(value));
@@ -12,7 +13,7 @@ export const validatePositiveNumber = (value: string): boolean =>
   validateNumber(value) && parseFloat(value) >= 0;
 
 export const validateRejectionThreshold = (value: string) =>
-  (validateInbounds(value) && validateInteger(value)) || value === '';
+  (validateInbounds(value) && validateIntegerString(value)) || value === '';
 
 export const validatePersistedStateKey = (key: string) =>
   key.startsWith('reduxPersist:') && !!/reduxPersist:(.*)/g.exec(key);
