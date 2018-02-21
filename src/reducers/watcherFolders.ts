f153b346cbc8c8fbf95e56ea6d3d42874f872399
Shovon Hasan <im.shovonhasan@gmail.com>
2018-02-21 15:21:26 -0500

    watcherFolders reducer now responds to DELETE_WATCHER_FOLDER action.

--- src/reducers/watcherFolders.ts
+++ src/reducers/watcherFolders.ts
@@ -1,5 +1,9 @@
 import { WatcherFolderAction } from '../actions/watcherFolders';
-import { WATCHER_FOLDER_EDIT, CREATE_WATCHER_FOLDER } from '../constants';
+import {
+  WATCHER_FOLDER_EDIT,
+  CREATE_WATCHER_FOLDER,
+  DELETE_WATCHER_FOLDER
+} from '../constants';
 import { WatcherFolder, Watcher } from '../types';
 import { Map } from 'immutable';
 import { DEFAULT_WATCHER_FOLDER_ID } from '../constants/misc';
@@ -20,6 +24,8 @@ export default (
   switch (action.type) {
     case CREATE_WATCHER_FOLDER:
       return state.set(action.payload.id, action.payload);
+    case DELETE_WATCHER_FOLDER:
+      return state.delete(action.folderId);
     case WATCHER_FOLDER_EDIT:
       return state.update(action.folderId, folder => ({
         ...folder,
