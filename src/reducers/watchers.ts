98639d9d57510fac88122d11aecbb964e5271762
Shovon Hasan <im.shovonhasan@gmail.com>
2018-02-21 15:27:08 -0500

    When a folder is deleted, all watchers assigned to that folder are deleted.

--- src/reducers/watchers.ts
+++ src/reducers/watchers.ts
@@ -1,11 +1,20 @@
 import { WatcherMap, Watcher } from '../types';
 import { WatcherAction } from '../actions/watcher';
-import { ADD_WATCHER, DELETE_WATCHER, EDIT_WATCHER_FIELD } from '../constants';
+import {
+  ADD_WATCHER,
+  DELETE_WATCHER,
+  EDIT_WATCHER_FIELD,
+  DELETE_WATCHER_FOLDER
+} from '../constants';
 import { Map } from 'immutable';
+import { DeleteWatcherFolder } from '../actions/watcherFolders';
 
 const initial: WatcherMap = Map<string, Watcher>();
 
-export default (state = initial, action: WatcherAction) => {
+export default (
+  state = initial,
+  action: WatcherAction | DeleteWatcherFolder
+) => {
   switch (action.type) {
     case ADD_WATCHER:
       return state.set(action.watcher.groupId, action.watcher);
@@ -16,6 +25,10 @@ export default (state = initial, action: WatcherAction) => {
         ...watcher,
         [action.field]: action.value
       }));
+    case DELETE_WATCHER_FOLDER:
+      return state.filterNot(
+        (watcher: Watcher) => watcher.folderId === action.folderId
+      );
     default:
       return state;
   }
