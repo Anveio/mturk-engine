ddd2cd1f25b079a9559a501cc35555c461116550
Shovon Hasan <im.shovonhasan@gmail.com>
2018-02-27 18:38:58 -0500

    Use new audio for new search result sound notification.

--- src/reducers/audioFiles.ts
+++ src/reducers/audioFiles.ts
@@ -2,7 +2,7 @@ import { AudioFiles } from '../types';
 
 const initial: AudioFiles = {
   audioNewSearch: new Audio(
-    'https://k003.kiwi6.com/hotlink/85iq6xu5ul/coins.ogg'
+    'https://k003.kiwi6.com/hotlink/w9aqj8az8t/ping.wav'
   )
 };
 
