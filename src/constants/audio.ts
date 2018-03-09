f79f36433b4459c1ca726a842185266c0ebea319
Shovon Hasan <im.shovonhasan@gmail.com>
2018-03-08 21:55:00 -0500

    Use louder & compressed audio files.

--- src/constants/audio.ts
+++ src/constants/audio.ts
@@ -1,4 +1,4 @@
 export enum AudioSources {
-  NEW_SEARCH_RESULT = 'https://k003.kiwi6.com/hotlink/w9aqj8az8t/ping.wav',
-  SUCCESSFUL_WATCHER_ACCEPT = 'https://k003.kiwi6.com/hotlink/7vmqepg960/horn.wav'
+  NEW_SEARCH_RESULT = 'https://k003.kiwi6.com/hotlink/bbgk5w2fhz/ping.ogg',
+  SUCCESSFUL_WATCHER_ACCEPT = 'https://k003.kiwi6.com/hotlink/sd6quj2ecs/horn.ogg'
 }
