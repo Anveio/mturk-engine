e807334bc7ef11235909fd1fe1f59026ae7d25e1
anveio <im.shovonhasan@gmail.com>
2018-03-30 06:54:37 -0400

    Remove references to legacy website.

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -11,15 +11,9 @@ export const baseTaskUrlWorker = 'https://worker.mturk.com/projects/';
 
 export const baseRequesterUrlWorker = 'https://worker.mturk.com/requesters/';
 
-export const baseContactUrlLegacy = 'https://www.mturk.com/mturk/contact?';
-
 export const baseContactUrlWorker =
   'https://worker.mturk.com/contact_requester/assignment_messages/new?';
 
-export const baseAcceptUrlLegacy =
-  'https://www.mturk.com/mturk/previewandaccept?groupId=';
 export const baseAcceptUrlWorker = 'https://worker.mturk.com/projects/';
 
-export const basePreviewUrlLegacy =
-  'https://www.mturk.com/mturk/preview?groupId=';
 export const basePreviewUrlWorker = 'https://worker.mturk.com/projects/';
--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -6,51 +6,21 @@ import {
 } from 'types';
 import {
   baseContactUrlWorker,
-  baseAcceptUrlLegacy,
   baseAcceptUrlWorker,
-  basePreviewUrlLegacy,
   basePreviewUrlWorker
 } from '../constants/urls';
 import { legacyDateFormatToContactDateFormat } from './dates';
 
-const noRedirectParam = '&doNotRedirect=true';
+export const generateAcceptUrl = (groupId: string) =>
+  `${baseAcceptUrlWorker}${groupId}/tasks/accept_random`;
 
-export const generateAcceptUrl = (groupId: string, legacy = false) =>
-  legacy
-    ? `${baseAcceptUrlLegacy}${groupId}${noRedirectParam}`
-    : `${baseAcceptUrlWorker}${groupId}/tasks/accept_random`;
+export const generatePreviewUrl = (groupId: string) =>
+  `${basePreviewUrlWorker}${groupId}/tasks`;
 
-export const generatePreviewUrl = (groupId: string, legacy = false) =>
-  legacy
-    ? `${basePreviewUrlLegacy}${groupId}${noRedirectParam}`
-    : `${basePreviewUrlWorker}${groupId}/tasks`;
-
-export const generateContinueWorkUrl = (hit: QueueItem, legacy = false) =>
-  legacy
-    ? `https://www.mturk.com/mturk/continue?hitId=${
-        hit.hitId
-      }${noRedirectParam}`
-    : `https://worker.mturk.com/projects/${hit.groupId}/tasks/${
-        hit.taskId
-      }?assignment_id=${hit.hitId}&ref=w_pl_prvw`;
-
-// const generateLegacyContactLink = (hit: HitDatabaseEntry) => {
-//   const { requester, id, title } = hit;
-//   return (
-//     baseContactUrlLegacy +
-//     'requesterId=' +
-//     requester.id +
-//     '&hitId=' +
-//     id +
-//     '&requesterName=' +
-//     requester.name +
-//     '&subject=Regarding Amazon Mechanical Turk HIT' +
-//     id +
-//     '&hitTitle=' +
-//     title +
-//     noRedirectParam
-//   );
-// };
+export const generateContinueWorkUrl = (hit: QueueItem) =>
+  `https://worker.mturk.com/projects/${hit.groupId}/tasks/${
+    hit.taskId
+  }?assignment_id=${hit.hitId}&ref=w_pl_prvw`;
 
 export const generateContactLink = (hit: WorkerHitDatabaseEntry) => {
   const { requester, id, assignmentId, title } = hit;
