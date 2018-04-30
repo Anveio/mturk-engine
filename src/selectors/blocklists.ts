8b2696038c6a5bfe9b221433f7778be311e7a45b
anveio <im.shovonhasan@gmail.com>
2018-04-30 07:38:37 -0400

    recentlyBlockedRequesterIds selector converts to a Set instead of an array.

--- src/selectors/blocklists.ts
+++ src/selectors/blocklists.ts
@@ -1,5 +1,10 @@
 import { createSelector } from 'reselect';
-import { HitBlockMap, BlockedHit } from '../types';
+import {
+  HitBlockMap,
+  BlockedHit,
+  RequesterBlockMap,
+  BlockedRequester
+} from '../types';
 import { hitBlocklistSelector, requesterBlocklistSelector } from './index';
 
 export const blockListsAreEmpty = createSelector(
@@ -23,4 +28,22 @@ export const recentlyBlockedHitIds = createSelector(
       .map((el: BlockedHit) => el.groupId)
       .slice(0, 20)
       .toArray()
+);
+
+export const sortedRequesterBlockList = createSelector(
+  [requesterBlocklistSelector],
+  (blockedRequesters: RequesterBlockMap) =>
+    blockedRequesters.sort(
+      (a: BlockedRequester, b: BlockedRequester) =>
+        +b.dateBlocked - +a.dateBlocked
+    )
+);
+
+export const recentlyBlockedRequesterIds = createSelector(
+  [sortedRequesterBlockList],
+  (blockedRequesters: RequesterBlockMap) =>
+    blockedRequesters
+      .map((el: BlockedRequester) => el.id)
+      .slice(0, 30)
+      .toSet()
 );
