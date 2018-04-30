ea9bfd8e56f1f39d2c81855aa72b1c759c5afb50
anveio <im.shovonhasan@gmail.com>
2018-04-30 07:36:21 -0400

    Add SweepMenu component.

---
+++ src/components/BlockList/SweepMenu.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { Menu, MenuDivider, MenuItem } from '@blueprintjs/core';
+import { Set } from 'immutable';
+
+interface Props {
+  readonly title: string;
+  readonly entries: {
+    readonly olderThanThirtyDays: Set<string>;
+    readonly olderThanSixtyDays: Set<string>;
+    readonly olderThanNinetyDays: Set<string>;
+  };
+}
+
+interface Handlers {
+  readonly onSweep: (ids: Set<string>) => void;
+}
+
+class SweepMenu extends React.Component<Props & Handlers, never> {
+  private handleClickForEntries = (entries: Set<string>) => () =>
+    this.props.onSweep(entries);
+
+  public render() {
+    const {
+      title,
+      entries: { olderThanThirtyDays, olderThanSixtyDays, olderThanNinetyDays }
+    } = this.props;
+    return (
+      <Menu>
+        <MenuDivider title={title} />
+        <MenuItem
+          icon="time"
+          onClick={this.handleClickForEntries(olderThanThirtyDays)}
+          text="Older than 30 days"
+        />
+        <MenuItem
+          icon="time"
+          onClick={this.handleClickForEntries(olderThanSixtyDays)}
+          text="Older than 60 days"
+        />
+        <MenuItem
+          icon="time"
+          onClick={this.handleClickForEntries(olderThanNinetyDays)}
+          text="Older than 90 days"
+        />
+      </Menu>
+    );
+  }
+}
+
+export default SweepMenu;
