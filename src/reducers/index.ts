68eb5790203258dd85e02d2ef7b188a821d9b3ab
anveio <im.shovonhasan@gmail.com>
2018-05-05 22:25:04 -0400

    Add index.ts bucket in reducers folder.

---
+++ src/reducers/index.ts
@@ -0,0 +1,63 @@
+import tab from './tab';
+import searchingActive from './searchingActive';
+import account from './account';
+import search from './search';
+import queue from './queue';
+import watchers from './watchers';
+import searchOptions from './searchOptions';
+import sortingOption from './sortingOption';
+import hitBlocklist from './hitBlocklist';
+import timeNextSearch from './timeNextSearch';
+import waitingForMturk from './waitingForMturk';
+import topticonSettings from './topticonSettings';
+import requesterBlocklist from './requesterBlocklist';
+import audioSettingsV1 from './audioSettings';
+import selectedHitDbDate from './selectedHitDbDate';
+import hitDatabase from './hitDatabase';
+import dailyEarningsGoal from './dailyEarningsGoal';
+import waitingForHitDbRefresh from './waitingForHitDbRefresh';
+import watcherTimers from './watcherTimers';
+import uploadedState from './uploadedState';
+import expandedSearchResults from './expandedSearchResults';
+import notificationSettings from './notificationSettings';
+import loggedSearchResults from './loggedSearchResults';
+import watcherTreeSettings from './watcherTreeSettings';
+import watcherFolders from './watcherFolders';
+import watcherStatistics from './watcherStatistics';
+import expandedWatcherFolderIds from './expandedWatcherFolderIds';
+import expandedQueueItems from './expandedQueueItems';
+import searchAudioEnabled from './searchAudioEnabled';
+import queueSortingOption from './queueSortingOption';
+
+export {
+  tab,
+  searchingActive,
+  account,
+  search,
+  queue,
+  watchers,
+  searchOptions,
+  sortingOption,
+  hitBlocklist,
+  timeNextSearch,
+  waitingForMturk,
+  topticonSettings,
+  requesterBlocklist,
+  audioSettingsV1,
+  selectedHitDbDate,
+  hitDatabase,
+  dailyEarningsGoal,
+  waitingForHitDbRefresh,
+  watcherTimers,
+  uploadedState,
+  expandedSearchResults,
+  notificationSettings,
+  loggedSearchResults,
+  watcherTreeSettings,
+  watcherFolders,
+  watcherStatistics,
+  expandedWatcherFolderIds,
+  expandedQueueItems,
+  searchAudioEnabled,
+  queueSortingOption,
+};
