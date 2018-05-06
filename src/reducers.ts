1c6fd909edb47587ffde070324caa910aaeabba4
anveio <im.shovonhasan@gmail.com>
2018-05-05 22:25:09 -0400

    Clean up imports in root reducer.

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,35 +1,37 @@
 import { RootState } from './types';
 import { combineReducers } from 'redux';
-import { default as tab } from './reducers/tab';
-import { default as searchingActive } from './reducers/searchingActive';
-import { default as account } from './reducers/account';
-import { default as search } from './reducers/search';
-import { default as queue } from './reducers/queue';
-import { default as watchers } from './reducers/watchers';
-import { default as searchOptions } from './reducers/searchOptions';
-import { default as sortingOption } from './reducers/sortingOption';
-import { default as hitBlocklist } from './reducers/hitBlocklist';
-import { default as timeNextSearch } from './reducers/timeNextSearch';
-import { default as waitingForMturk } from './reducers/waitingForMturk';
-import { default as topticonSettings } from './reducers/topticonSettings';
-import { default as requesterBlocklist } from './reducers/requesterBlocklist';
-import { default as audioSettingsV1 } from './reducers/audioSettings';
-import { default as selectedHitDbDate } from './reducers/selectedHitDbDate';
-import { default as hitDatabase } from './reducers/hitDatabase';
-import { default as dailyEarningsGoal } from './reducers/dailyEarningsGoal';
-import { default as waitingForHitDbRefresh } from './reducers/waitingForHitDbRefresh';
-import { default as watcherTimers } from './reducers/watcherTimers';
-import { default as uploadedState } from './reducers/uploadedState';
-import { default as expandedSearchResults } from './reducers/expandedSearchResults';
-import { default as notificationSettings } from './reducers/notificationSettings';
-import { default as loggedSearchResults } from './reducers/loggedSearchResults';
-import { default as watcherTreeSettings } from './reducers/watcherTreeSettings';
-import { default as watcherFolders } from './reducers/watcherFolders';
-import { default as watcherStatistics } from './reducers/watcherStatistics';
-import { default as expandedWatcherFolderIds } from './reducers/expandedWatcherFolderIds';
-import { default as expandedQueueItems } from './reducers/expandedQueueItems';
-import { default as searchAudioEnabled } from './reducers/searchAudioEnabled';
-import { default as queueSortingOption } from './reducers/queueSortingOption';
+import {
+  tab,
+  searchingActive,
+  account,
+  search,
+  queue,
+  watchers,
+  searchOptions,
+  sortingOption,
+  hitBlocklist,
+  timeNextSearch,
+  waitingForMturk,
+  topticonSettings,
+  requesterBlocklist,
+  audioSettingsV1,
+  selectedHitDbDate,
+  hitDatabase,
+  dailyEarningsGoal,
+  waitingForHitDbRefresh,
+  watcherTimers,
+  uploadedState,
+  expandedSearchResults,
+  notificationSettings,
+  loggedSearchResults,
+  watcherTreeSettings,
+  watcherFolders,
+  watcherStatistics,
+  expandedWatcherFolderIds,
+  expandedQueueItems,
+  searchAudioEnabled,
+  queueSortingOption
+} from './reducers/index';
 
 export const rootReducer = combineReducers<RootState>({
   tab,
