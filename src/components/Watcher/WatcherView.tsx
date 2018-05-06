5a3dac442b4e8c5a4f99c86f6937b27822e76c2f
anveio <im.shovonhasan@gmail.com>
2018-05-06 06:40:51 -0400

    WatcherView no longer receives the map of all watchers.

--- src/components/Watcher/WatcherView.tsx
+++ src/components/Watcher/WatcherView.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { connect, Dispatch } from 'react-redux';
 import { Stack } from '@shopify/polaris';
-import { RootState, Watcher, Primitive } from '../../types';
+import { RootState, Primitive } from '../../types';
 import {
   scheduleWatcher,
   cancelNextWatcherTick,
@@ -16,14 +16,12 @@ import WatcherSettings from './WatcherSettings';
 import InfoCallout from './InfoCallout';
 import WatcherActions from './WatcherActions';
 import WatcherProgressBar from './WatcherProgressBar';
-import { normalizedWatchers } from '../../selectors/watchers';
 
 interface OwnProps {
   readonly watcherId: string;
 }
 
 interface Props {
-  readonly watcher: Watcher;
   readonly watcherActive: boolean;
 }
 
@@ -42,7 +40,7 @@ class WatcherView extends React.PureComponent<
   OwnProps & Props & Handlers,
   never
 > {
-  private handleDelete = () => this.props.onDelete(this.props.watcher.groupId);
+  private handleDelete = () => this.props.onDelete(this.props.watcherId);
 
   private handleToggle = () => {
     const { watcherActive, onCancel, watcherId, onSchedule } = this.props;
@@ -50,30 +48,29 @@ class WatcherView extends React.PureComponent<
   };
 
   public render() {
-    const { watcher, watcherActive } = this.props;
+    const { watcherId, watcherActive } = this.props;
 
     return (
       <Stack vertical>
         <WatcherHeading
-          title={watcher.title}
+          watcherId={watcherId}
           onChange={(value: string) =>
             this.props.onEdit(this.props.watcherId, 'title', value)
           }
         />
         <WatcherInfo
-          id={watcher.groupId}
-          description={watcher.description}
+          watcherId={watcherId}
           onChangeDescription={(value: string) =>
             this.props.onEdit(this.props.watcherId, 'description', value)
           }
         />
         <WatcherSettings
-          watcher={watcher}
+          watcherId={watcherId}
           onEdit={this.props.onEdit}
           onToggle={this.handleToggle}
         />
         <InfoCallout />
-        <WatcherProgressBar id={watcher.groupId} />
+        <WatcherProgressBar id={watcherId} />
         <WatcherActions
           watcherActive={watcherActive}
           onDelete={this.handleDelete}
@@ -85,7 +82,6 @@ class WatcherView extends React.PureComponent<
 }
 
 const mapState = (state: RootState, ownProps: OwnProps): Props => ({
-  watcher: normalizedWatchers(state).get(ownProps.watcherId),
   watcherActive: state.watcherTimers.has(ownProps.watcherId)
 });
 
