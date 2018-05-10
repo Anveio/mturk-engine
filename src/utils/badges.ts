5a6b0ba71060e127b7980e13b95334c253f63ad5
anveio <im.shovonhasan@gmail.com>
2018-05-10 16:43:21 -0400

    Update badge utility functions to use Polaris 2.0 Badge API.

--- src/utils/badges.ts
+++ src/utils/badges.ts
@@ -1,40 +1,7 @@
 import { HitStatus } from '../types';
-import { BadgeDescriptor } from '@shopify/polaris/types/components/ResourceList/Item';
 import { Status } from '@shopify/polaris/types/components/Badge/Badge';
-// import { BadgeProps } from '@shopify/polaris';
 
-const noTOBadge: BadgeDescriptor = {
-  content: 'No T.O.',
-  status: '' as Status
-};
-
-export const generateTOpticonBadge = (
-  averageScore: number | null
-): BadgeDescriptor[] => {
-  if (!averageScore) {
-    return [noTOBadge];
-  }
-
-  const status = assignScoreColor(averageScore) as Status;
-  const content = generateContentString(averageScore);
-
-  return [
-    {
-      status,
-      content
-    }
-  ];
-};
-
-const generateContentString = (average: number | null) => {
-  return !average ? 'No T.O.' : `${average.toFixed(2)} T.O.`;
-};
-
-const assignScoreColor = (score: number | null): Status | null => {
-  if (!score) {
-    return null;
-  }
-
+export const assignScoreColor = (score: number): Status => {
   if (score < 2) {
     return 'warning';
   } else if (score < 3) {
@@ -46,19 +13,19 @@ const assignScoreColor = (score: number | null): Status | null => {
   }
 };
 
-export const generateHitStatusBadge = (status: HitStatus): BadgeDescriptor => {
+export const generateHitStatusBadge = (status: HitStatus): Status => {
   switch (status) {
     case 'Paid':
-      return { content: 'Paid', status: 'success' };
+      return 'success';
     case 'Approved':
     case 'Pending Payment':
-      return { content: 'Approved', status: 'success' };
+      return 'success';
     case 'Rejected':
-      return { content: 'Rejected', status: 'warning' };
+      return 'warning';
     case 'Submitted':
     case 'Pending Approval':
-      return { content: 'Pending', status: 'info' };
+      return 'info';
     default:
-      return { content: 'Pending', status: 'info' };
+      return 'info';
   }
 };
