c0dc41e605fc649c5b6c37b22580a4dd2613599f
anveio <im.shovonhasan@gmail.com>
2018-05-10 16:05:39 -0400

    Add PolarisProvider to GenericWaitingToast.

--- src/components/Toasts.tsx
+++ src/components/Toasts.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
-import { Stack, Spinner, TextContainer } from '@shopify/polaris';
+import {
+  Stack,
+  Spinner,
+  TextContainer,
+  AppProvider as PolarisProvider
+} from '@shopify/polaris';
 
 interface Props {
   readonly message: string;
 }
 
 export const GenericWaitingToast = ({ message }: Props) => (
-  <Stack vertical={false} wrap={false}>
-    <Spinner
-      size="small"
-      accessibilityLabel="Waiting for a response from MTurk."
-    />
-    <TextContainer>{message}</TextContainer>
-  </Stack>
+  <PolarisProvider>
+    <Stack vertical={false} wrap={false}>
+      <Spinner
+        size="small"
+        accessibilityLabel="Waiting for a response from MTurk."
+      />
+      <TextContainer>{message}</TextContainer>
+    </Stack>
+  </PolarisProvider>
 );
