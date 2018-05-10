7637750c30fb3cd99c6bacd413bb187b2cf7ac58
anveio <im.shovonhasan@gmail.com>
2018-05-10 16:42:22 -0400

    InfoContainer now renders the turkopticon score badge.

--- src/components/SearchCard/InfoContainer.tsx
+++ src/components/SearchCard/InfoContainer.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import { Stack, DisplayText, Caption } from '@shopify/polaris';
 import { formatAsUsd } from '../../utils/formatting';
+import TOpticonBadge from './TOpticonBadge';
+import { SearchResult } from 'types';
 
 interface Props {
-  readonly reward: number;
-  readonly batchSize: number;
+  readonly hit: SearchResult;
 }
 
-const InfoContainer: React.SFC<Props> = ({ reward, batchSize }) => (
-  <Stack vertical={false} spacing={'tight'} alignment="baseline">
+const InfoContainer: React.SFC<Props> = ({
+  hit: { groupId, reward, batchSize, requester }
+}) => (
+  <Stack vertical={false} alignment="baseline">
+    <TOpticonBadge groupId={groupId} />
     <Caption>{batchSize} available</Caption>
     <DisplayText size="small">{formatAsUsd(reward)}</DisplayText>
   </Stack>
