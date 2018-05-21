7911d8a673919aead9e9933a277772bf2bfd2fc5
anveio <im.shovonhasan@gmail.com>
2018-05-20 20:04:42 -0400

    Add action creators for UPDATE_DB_STATUS_FILTERS action types.

--- src/actions/databaseFilterSettings.ts
+++ src/actions/databaseFilterSettings.ts
@@ -1,9 +1,27 @@
 import { updateValue } from './updateValue';
-import { UPDATE_DB_SEARCH_TERM } from '../constants';
+import { UPDATE_DB_SEARCH_TERM, UPDATE_DB_STATUS_FILTERS } from '../constants';
+import { StatusFilterType } from 'types';
+import { Set } from 'immutable';
 
 export interface UpdateDatabaseSearchTerm {
   readonly type: UPDATE_DB_SEARCH_TERM;
   readonly data: string;
 }
 
+export interface UpdateDatabaseStatusFilters {
+  readonly type: UPDATE_DB_STATUS_FILTERS;
+  readonly data: Set<StatusFilterType>;
+}
+
+export type DatabaseFilterAction =
+  | UpdateDatabaseSearchTerm
+  | UpdateDatabaseStatusFilters;
+
 export const changeSearchTerm = updateValue<string>(UPDATE_DB_SEARCH_TERM);
+
+export const changeFilters = (
+  data: Set<StatusFilterType>
+): UpdateDatabaseStatusFilters => ({
+  type: UPDATE_DB_STATUS_FILTERS,
+  data
+});
