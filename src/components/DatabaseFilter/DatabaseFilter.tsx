afd3df61689d1ba999c6f72fc1a71164c78d90bd
anveio <im.shovonhasan@gmail.com>
2018-05-21 16:37:36 -0400

    DatabaseFilter results are now paginated.

--- src/components/DatabaseFilter/DatabaseFilter.tsx
+++ src/components/DatabaseFilter/DatabaseFilter.tsx
@@ -1,76 +1,82 @@
 import * as React from 'react';
-import { Card, ResourceList, Pagination } from '@shopify/polaris';
+import { Card } from '@shopify/polaris';
 import { connect } from 'react-redux';
-import { RootState, HitId, StatusFilterType } from 'types';
-import CompletedHitItem from '../SelectedHitDate/CompletedHitItem';
+import { RootState, HitId } from 'types';
 import { hitDatabaseFilteredBySearchTerm } from 'selectors/databaseFilterSettings';
-import {
-  changeSearchTerm,
-  changeFilters
-} from 'actions/databaseFilterSettings';
-import { Set } from 'immutable';
-import {
-  statusFiltersToAppliedFilterArray,
-  availableFilters,
-  appliedFiltersToStatusFilterTypeSet,
-  AppliedHitDatabaseFilter
-} from 'utils/databaseFilter';
+import { Iterable } from 'immutable';
+import DatabaseFilterPagination, {
+  DatabaseFilterPaginationProps
+} from './DatabaseFilterPagination';
+import { RESULTS_PER_PAGE } from 'constants/misc';
+import ResultsList from './ResultsList';
 
 interface Props {
-  readonly hitIds: HitId[];
-  readonly searchTerm: string;
-  readonly statusFilters: Set<StatusFilterType>;
+  readonly hitIds: Iterable<HitId, HitId>;
 }
 
-interface Handlers {
-  readonly onSearchChange: (value: string) => void;
-  readonly onFilterChange: (filters: Set<StatusFilterType>) => void;
+interface State {
+  readonly page: number;
+  readonly maxPage: number;
 }
 
-// tslint:disable:no-console
+class DatabaseFilter extends React.Component<Props, State> {
+  public readonly state: State = { page: 0, maxPage: 0 };
 
-class DatabaseFilter extends React.Component<Props & Handlers, never> {
-  private handleFilterChange = (filters: AppliedHitDatabaseFilter[]) => {
-    const newFilters = appliedFiltersToStatusFilterTypeSet(filters);
-    this.props.onFilterChange(newFilters);
-  };
+  shouldComponentUpdate(nextProps: Props) {
+    return nextProps.hitIds.equals(this.props.hitIds);
+  }
+
+  static getDerivedStateFromProps(nextProps: Props): State {
+    return {
+      page: 0,
+      maxPage: DatabaseFilter.calculateMaxPage(
+        nextProps.hitIds.size,
+        RESULTS_PER_PAGE
+      )
+    };
+  }
+
+  private static calculateMaxPage = (
+    numResults: number,
+    resultsPerPage: number
+  ) => Math.ceil(numResults / resultsPerPage) - 1;
+
+  private calculateHasNext = (page: number, maxPage: number) => page < maxPage;
+  private calculateHasPrevious = (page: number) => page > 0;
+
+  private onNext = () =>
+    this.setState((prevState: State) => ({
+      page: Math.min(prevState.page + 1, this.state.maxPage)
+    }));
+
+  private onPrevious = () =>
+    this.setState((prevState: State) => ({
+      page: Math.max(prevState.page - 1, 0)
+    }));
 
   public render() {
-    const filters = statusFiltersToAppliedFilterArray(this.props.statusFilters);
+    const hasNext = this.calculateHasNext(this.state.page, this.state.maxPage);
+    const hasPrevious = this.calculateHasPrevious(this.state.page);
+
+    const paginationProps: DatabaseFilterPaginationProps = {
+      hasNext,
+      hasPrevious,
+      onNext: this.onNext,
+      onPrevious: this.onPrevious,
+      shouldRender: this.props.hitIds.size > RESULTS_PER_PAGE
+    };
+
     return (
       <Card title="Search your HIT database">
-        <ResourceList
-          showHeader
-          filterControl={
-            <ResourceList.FilterControl
-              searchValue={this.props.searchTerm}
-              onSearchChange={this.props.onSearchChange}
-              filters={availableFilters}
-              appliedFilters={filters}
-              onFiltersChange={this.handleFilterChange}
-            />
-          }
-          resourceName={{ singular: 'HIT', plural: 'HITs' }}
-          items={this.props.hitIds}
-          renderItem={(id: string) => <CompletedHitItem id={id} />}
-        />
-        <Pagination hasNext={this.props.hitIds.length > 20} />
+        <ResultsList page={this.state.page} />
+        <DatabaseFilterPagination {...paginationProps} />
       </Card>
     );
   }
 }
 
 const mapState = (state: RootState): Props => ({
-  searchTerm: state.databaseFilterSettings.searchTerm,
-  statusFilters: state.databaseFilterSettings.statusFilters,
   hitIds: hitDatabaseFilteredBySearchTerm(state)
-    .toArray()
-    .slice(0, 10)
 });
 
-const mapDispatch: Handlers = {
-  onSearchChange: changeSearchTerm,
-  onFilterChange: changeFilters
-};
-
-export default connect(mapState, mapDispatch)(DatabaseFilter);
+export default connect(mapState)(DatabaseFilter);
