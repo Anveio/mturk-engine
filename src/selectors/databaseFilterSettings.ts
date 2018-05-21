5ec8a96dde9826044f6750f804d0b8c1f90f0882
anveio <im.shovonhasan@gmail.com>
2018-05-21 11:41:55 -0400

    Add selector for filtering HITs by status.

--- src/selectors/databaseFilterSettings.ts
+++ src/selectors/databaseFilterSettings.ts
@@ -1,11 +1,38 @@
-import { hitDatabaseSelector, databaseFIlterSettingsSelector } from './index';
+import { hitDatabaseSelector, databaseFilterSettingsSelector } from './index';
 import { createSelector } from 'reselect';
-import { HitDatabaseEntry } from 'types';
+import { HitDatabaseEntry, HitDatabaseMap, StatusFilterType } from 'types';
+import { filterBy } from 'utils/databaseFilter';
+import { Map } from 'immutable';
 
 export const hitDatabaseFilteredBySearchTerm = createSelector(
-  [hitDatabaseSelector, databaseFIlterSettingsSelector],
+  [hitDatabaseSelector, databaseFilterSettingsSelector],
   (hitDatabase, { searchTerm }) =>
     hitDatabase
       .filter((hit: HitDatabaseEntry) => hit.title.search(searchTerm) !== -1)
       .map((el: HitDatabaseEntry) => el.id)
+);
+
+export const hitDatabaseFilteredByStatus = createSelector(
+  [hitDatabaseSelector, databaseFilterSettingsSelector],
+  (hitDatabase, { statusFilters }) => {
+    if (statusFilters.isEmpty()) {
+      return hitDatabase;
+    }
+
+    const filterFunction = filterBy(hitDatabase);
+
+    const resultsMaps = statusFilters.reduce(
+      (acc: HitDatabaseMap[], cur: StatusFilterType) =>
+        acc.concat(filterFunction(cur)),
+      []
+    );
+
+    return resultsMaps
+      .reduce(
+        (acc: HitDatabaseMap, cur: HitDatabaseMap) => acc.merge(cur),
+        Map()
+      )
+      .map((el: HitDatabaseEntry) => el.id)
+      .toArray();
+  }
 );
