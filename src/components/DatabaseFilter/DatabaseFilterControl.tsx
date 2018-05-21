c1f5de9822231e0b608e129398a051b3e09c9c71
anveio <im.shovonhasan@gmail.com>
2018-05-21 18:21:15 -0400

    Extract ResourceList.FilterControl in ResultsList to separate component.

---
+++ src/components/DatabaseFilter/DatabaseFilterControl.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { ResourceList } from '@shopify/polaris';
+import { StatusFilterType, RootState } from 'types';
+import {
+  availableFilters,
+  statusFiltersToAppliedFilterArray,
+  AppliedHitDatabaseFilter,
+  appliedFiltersToStatusFilterTypeArray
+} from 'utils/databaseFilter';
+import { connect } from 'react-redux';
+import {
+  changeSearchTerm,
+  changeFilters
+} from 'actions/databaseFilterSettings';
+
+interface Props {
+  readonly searchTerm: string;
+  readonly statusFilters: StatusFilterType[];
+}
+
+interface Handlers {
+  readonly onSearchChange: (value: string) => void;
+  readonly onFilterChange: (filters: StatusFilterType[]) => void;
+}
+
+class DatabaseFilterControl extends React.Component<Props & Handlers, never> {
+  private handleFilterChange = (filters: AppliedHitDatabaseFilter[]) => {
+    const newFilters = appliedFiltersToStatusFilterTypeArray(filters);
+    this.props.onFilterChange(newFilters);
+  };
+
+  public render() {
+    const filters = statusFiltersToAppliedFilterArray(this.props.statusFilters);
+
+    return (
+      <ResourceList.FilterControl
+        searchValue={this.props.searchTerm}
+        onSearchChange={this.props.onSearchChange}
+        filters={availableFilters}
+        appliedFilters={filters}
+        onFiltersChange={this.handleFilterChange}
+      />
+    );
+  }
+}
+
+const mapState = (state: RootState): Props => ({
+  searchTerm: state.databaseFilterSettings.searchTerm,
+  statusFilters: state.databaseFilterSettings.statusFilters
+});
+
+const mapDispatch: Handlers = {
+  onSearchChange: changeSearchTerm,
+  onFilterChange: changeFilters
+};
+
+export default connect(mapState, mapDispatch)(DatabaseFilterControl);
--- src/components/DatabaseFilter/ResultsList.tsx
+++ src/components/DatabaseFilter/ResultsList.tsx
@@ -1,61 +1,31 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { RootState, HitId, StatusFilterType } from 'types';
+import { RootState, HitId } from 'types';
 import { ResourceList } from '@shopify/polaris';
 import { DATABASE_FILTER_RESULTS_PER_PAGE } from 'constants/misc';
 import CompletedHitItem from '../SelectedHitDate/CompletedHitItem';
-import {
-  statusFiltersToAppliedFilterArray,
-  availableFilters,
-  appliedFiltersToStatusFilterTypeArray,
-  AppliedHitDatabaseFilter
-} from 'utils/databaseFilter';
 import { hitDatabaseFilteredBySearchTerm } from 'selectors/databaseFilterSettings';
-import {
-  changeSearchTerm,
-  changeFilters
-} from 'actions/databaseFilterSettings';
+import DatabaseFilterControl from './DatabaseFilterControl';
 
 interface Props {
-  readonly searchTerm: string;
-  readonly statusFilters: StatusFilterType[];
   readonly hitIds: HitId[];
-}
-
-interface Handlers {
-  readonly onSearchChange: (value: string) => void;
-  readonly onFilterChange: (filters: StatusFilterType[]) => void;
 }
 
 interface OwnProps {
   readonly page: number;
 }
 
-class ResultsList extends React.Component<Props & OwnProps & Handlers, never> {
-  private handleFilterChange = (filters: AppliedHitDatabaseFilter[]) => {
-    const newFilters = appliedFiltersToStatusFilterTypeArray(filters);
-    this.props.onFilterChange(newFilters);
-  };
-
+class ResultsList extends React.PureComponent<Props & OwnProps, never> {
   public render() {
     const { hitIds, page } = this.props;
     const start = DATABASE_FILTER_RESULTS_PER_PAGE * page;
     const end = start + DATABASE_FILTER_RESULTS_PER_PAGE;
     const itemsToShow = hitIds.slice(start, end);
-    const filters = statusFiltersToAppliedFilterArray(this.props.statusFilters);
 
     return (
       <ResourceList
         showHeader
-        filterControl={
-          <ResourceList.FilterControl
-            searchValue={this.props.searchTerm}
-            onSearchChange={this.props.onSearchChange}
-            filters={availableFilters}
-            appliedFilters={filters}
-            onFiltersChange={this.handleFilterChange}
-          />
-        }
+        filterControl={<DatabaseFilterControl />}
         resourceName={{ singular: 'HIT', plural: 'HITs' }}
         items={itemsToShow}
         renderItem={(id: string) => <CompletedHitItem id={id} />}
@@ -65,14 +35,7 @@ class ResultsList extends React.Component<Props & OwnProps & Handlers, never> {
 }
 
 const mapState = (state: RootState): Props => ({
-  searchTerm: state.databaseFilterSettings.searchTerm,
-  statusFilters: state.databaseFilterSettings.statusFilters,
   hitIds: hitDatabaseFilteredBySearchTerm(state).toArray()
 });
 
-const mapDispatch: Handlers = {
-  onSearchChange: changeSearchTerm,
-  onFilterChange: changeFilters
-};
-
-export default connect(mapState, mapDispatch)(ResultsList);
+export default connect(mapState)(ResultsList);
