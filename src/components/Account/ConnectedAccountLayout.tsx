53605b9608a6bb3ad76b4c8055ebfaf0621c0afe
anveio <im.shovonhasan@gmail.com>
2018-05-20 20:09:18 -0400

    Add DatabaseFilter component to ConnectedAccountLayout.

--- src/components/Account/ConnectedAccountLayout.tsx
+++ src/components/Account/ConnectedAccountLayout.tsx
@@ -5,6 +5,7 @@ import Calendar from '../Calendar/Calendar';
 import EarningsSummary from './EarningsSummary';
 import InformationDisclaimer from './InformationDisclaimer';
 import SelectedHitDate from '../SelectedHitDate/SelectedHitDate';
+import DatabaseFilter from '../DatabaseFilter/DatabaseFilter';
 
 class ConnectedAccountLayout extends React.PureComponent<{}, never> {
   public render() {
@@ -13,21 +14,16 @@ class ConnectedAccountLayout extends React.PureComponent<{}, never> {
         <Layout.Section secondary>
           <UserInfo />
         </Layout.Section>
-
         <Layout.Section secondary>
           <EarningsSummary />
         </Layout.Section>
-
         <InformationDisclaimer />
-
         <Layout.Section>
           <Calendar />
         </Layout.Section>
-
-        {/* <Layout.Section>
+        <Layout.Section>
           <DatabaseFilter />
-        </Layout.Section> */}
-
+        </Layout.Section>}
         <Layout.Section>
           <SelectedHitDate />
         </Layout.Section>
