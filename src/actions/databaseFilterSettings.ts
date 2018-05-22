ddb9761e2a6ffb944e04bc631701ba82775cc48d
anveio <im.shovonhasan@gmail.com>
2018-05-21 20:20:05 -0400

    Add changeFilterSortOrder action creator.

--- src/actions/databaseFilterSettings.ts
+++ src/actions/databaseFilterSettings.ts
@@ -1,6 +1,10 @@
 import { updateValue } from './updateValue';
-import { UPDATE_DB_SEARCH_TERM, UPDATE_DB_STATUS_FILTERS } from '../constants';
-import { StatusFilterType } from 'types';
+import {
+  UPDATE_DB_SEARCH_TERM,
+  UPDATE_DB_STATUS_FILTERS,
+  UPDATE_DB_SORT_ORDER
+} from '../constants';
+import { StatusFilterType, FilterOrderType } from 'types';
 
 export interface UpdateDatabaseSearchTerm {
   readonly type: UPDATE_DB_SEARCH_TERM;
@@ -12,11 +16,24 @@ export interface UpdateDatabaseStatusFilters {
   readonly data: StatusFilterType[];
 }
 
+export interface UpdateDatabaseSortOrder {
+  readonly type: UPDATE_DB_SORT_ORDER;
+  readonly data: FilterOrderType;
+}
+
 export type DatabaseFilterAction =
   | UpdateDatabaseSearchTerm
-  | UpdateDatabaseStatusFilters;
+  | UpdateDatabaseStatusFilters
+  | UpdateDatabaseSortOrder;
 
 export const changeSearchTerm = updateValue<string>(UPDATE_DB_SEARCH_TERM);
+
+export const changeFilterSortOrder = (
+  data: FilterOrderType
+): UpdateDatabaseSortOrder => ({
+  type: UPDATE_DB_SORT_ORDER,
+  data
+});
 
 export const changeFilters = (
   data: StatusFilterType[]
