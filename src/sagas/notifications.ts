529103638e7ec9b3378b50de6e919f731a527866
anveio <im.shovonhasan@gmail.com>
2018-05-23 07:36:32 -0400

    Move notification display logic to saga from component tree.

--- src/sagas/notifications.ts
+++ src/sagas/notifications.ts
@@ -1,11 +1,12 @@
 import { delay } from 'redux-saga';
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import {
   NotificationPermissionRequest,
   NotificationPermissionUpdate,
   notificationPermissionFailure,
   notificationPermissionSuccess,
-  SendNotification
+  SendNotification,
+  sendNotification
 } from '../actions/notifications';
 import {
   requestNotificationPermission,
@@ -15,6 +16,10 @@ import {
   notificationPermissionGrantedToast,
   notificationPermissionBlockedToast
 } from '../utils/toaster';
+import { SearchSuccess } from 'actions/search';
+import { topThreePayingResultsSuitableForNotification } from 'selectors/notificationSettings';
+import { SearchResults, NotificationSettings } from 'types';
+import { notificationSettingsSelector } from 'selectors';
 
 export function* resolveNotificationPermissionRequest(
   action: NotificationPermissionRequest
@@ -41,6 +46,21 @@ export function* resolveNotificationPermissionRequest(
     }
   } catch (e) {
     yield put<NotificationPermissionUpdate>(notificationPermissionFailure());
+  }
+}
+
+export function* sendNotificationForSearchResult(action: SearchSuccess) {
+  const resultsToSendNotificationsFor: SearchResults = yield select(
+    topThreePayingResultsSuitableForNotification
+  );
+  const { enabled, durationInSeconds }: NotificationSettings = yield select(
+    notificationSettingsSelector
+  );
+
+  if (enabled) {
+    for (const result of resultsToSendNotificationsFor.toArray()) {
+      yield put<SendNotification>(sendNotification(result, durationInSeconds));
+    }
   }
 }
 
