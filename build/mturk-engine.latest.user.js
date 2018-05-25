a1ad456676789337b850ab695b3104a7936d9876
anveio <im.shovonhasan@gmail.com>
2018-05-25 05:31:36 -0400

    1.8.0

---
+++ build/mturk-engine.latest.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Mturk Engine
+// @namespace    https://github.com/Anveio/mturk-engine/
+// @version      1.8.0
+// @description  Earn money more efficiently on Amazon's Mechanical Turk work platform.
+// @author       Anveio (Shovon Hasan)
+// @match        https://worker.mturk.com/?mturkengine
+// @match        https://www.mturk.com/?mturkengine
+// @require      https://cdn.rawgit.com/Anveio/mturk-engine/2f6c82c4c00365dd28e191d4d262010b523dede5/build/mturk-engine.latest.raw.user.js
+// @grant        none
+// ==/UserScript==
+
+/**
+ * After downloading this script visit https://worker.mturk.com/?mturkengine
+ *
+ * Mturk Engine is free and open source. Visit this project's github page at: https://github.com/Anveio/mturk-engine
+ * There you can view the source code, post issues, submit changes, suggest features, and download the latest version.
+ * Changelogs are available at: https://github.com/Anveio/mturk-engine/releases
+ */
