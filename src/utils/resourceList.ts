1a647eff278e8833c02e06edb25d5efc5df46665
anveio <im.shovonhasan@gmail.com>
2018-05-25 04:34:57 -0400

    Delete unused resourcelist utility function.

--- src/utils/resourceList.ts
+++
@@ -1,6 +0,0 @@
-import { RESOURCE_LIST_ITEM_CLASS } from 'constants/misc';
-
-export const clickDidNotOccurOnActionButton = (
-  e: React.MouseEvent<HTMLDivElement>
-): boolean =>
-  (e.target as Element).getAttribute('class') === RESOURCE_LIST_ITEM_CLASS;
