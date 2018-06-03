57fb5b6275ec665f92b61c48884481cc530af88b
anveio <im.shovonhasan@gmail.com>
2018-06-03 12:41:56 -0400

    generate-release script now also generates CDN template file.

--- generate-release/utils.ts
+++ generate-release/utils.ts
@@ -1,16 +1,21 @@
 import * as fs from 'fs-extra';
 
 const constants = {
-  BUILD_FILE_FILEPATH: '../build/mturk-engine.latest.raw.user.js',
+  RAW_BUILD_FILE_FILEPATH: '../build/mturk-engine.latest.raw.user.js',
+  CDN_BUILD_FILE_FILEPATH: '../build/mturk-engine.latest.user.js',
   VERSION_NUMBER: process.argv[2]
 };
 
 export const generateRelease = async () => {
   try {
-    await generateBuildFile();
-
     // These things can happen concurrently.
-    await Promise.all([deleteStaticDirectory(), deleteExtraneousFiles()]);
+    await Promise.all([
+      createRawBuildFile(),
+      createCdnTemplate(),
+      deleteExtraneousFiles()
+    ]);
+
+    await deleteStaticDirectory();
   } catch (e) {
     // tslint:disable-next-line:no-console
     console.log(e);
@@ -18,23 +23,12 @@ export const generateRelease = async () => {
   }
 };
 
-const generateBuildFile = async () => {
-  const sourceCodeFile = await findSourceCodeFile();
-  await fs.createFile(constants.BUILD_FILE_FILEPATH);
-  await fs.writeFile(
-    constants.BUILD_FILE_FILEPATH,
-    generateUserScriptBoilerplate()
-  );
-
-  const sourceCode = await fs.readFile(`../build/static/js/${sourceCodeFile}`);
-
-  await fs.appendFile(constants.BUILD_FILE_FILEPATH, sourceCode);
-};
-
 const findSourceCodeFile = async () => {
   const files = await fs.readdir('../build/static/js');
   return files.find(filePath => !filePath.includes('.map'));
 };
+
+const deleteStaticDirectory = () => fs.remove('../build/static');
 
 const deleteExtraneousFiles = () =>
   Promise.all([
@@ -43,11 +37,27 @@ const deleteExtraneousFiles = () =>
     fs.unlink('../build/index.html')
   ]);
 
-const deleteStaticDirectory = () => fs.remove('../build/static');
+const createRawBuildFile = async () => {
+  const sourceCodeFile = await findSourceCodeFile();
+  await fs.createFile(constants.RAW_BUILD_FILE_FILEPATH);
+  await fs.writeFile(
+    constants.RAW_BUILD_FILE_FILEPATH,
+    rawUserScriptBoilerPlate
+  );
+  const sourceCode = await fs.readFile(`../build/static/js/${sourceCodeFile}`);
+  await fs.appendFile(constants.RAW_BUILD_FILE_FILEPATH, sourceCode);
+};
+
+const createCdnTemplate = async () => {
+  await fs.createFile(constants.CDN_BUILD_FILE_FILEPATH);
+  await fs.writeFile(
+    constants.CDN_BUILD_FILE_FILEPATH,
+    cdnUserScriptBoilerPlate
+  );
+};
 
 // tslint:disable:max-line-length
-const generateUserScriptBoilerplate = () =>
-  `// ==UserScript==
+const rawUserScriptBoilerPlate = `// ==UserScript==
 // @name         Mturk Engine (Raw)
 // @namespace    https://github.com/Anveio/mturk-engine/
 // @version      ${constants.VERSION_NUMBER}
@@ -56,6 +66,29 @@ const generateUserScriptBoilerplate = () =>
 // @license      MIT
 // @match        https://worker.mturk.com/?mturkengine
 // @match        https://www.mturk.com/?mturkengine
+// @grant        none
+// ==/UserScript==
+
+/**
+ * After downloading this script visit https://worker.mturk.com/?mturkengine
+ *
+ * Mturk Engine is free and open source. Visit this project's github page at: https://github.com/Anveio/mturk-engine
+ * There you can view the source code, post issues, submit changes, suggest features, and download the latest version.
+ * Changelogs are available at: https://github.com/Anveio/mturk-engine/releases
+ */
+
+`;
+
+const cdnUserScriptBoilerPlate = `// ==UserScript==
+// @name         Mturk Engine
+// @namespace    https://github.com/Anveio/mturk-engine/
+// @version      ${constants.VERSION_NUMBER}
+// @description  Earn money more efficiently on Amazon's Mechanical Turk work platform.
+// @author       Anveio (Shovon Hasan)
+// @license      MIT
+// @match        https://worker.mturk.com/?mturkengine
+// @match        https://www.mturk.com/?mturkengine
+// @require      [[[ Replace with rawgit URL. ]]]
 // @grant        none
 // ==/UserScript==
 
