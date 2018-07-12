0de313cc96cea0d7a80c6770533ec5d33c25b14e
anveio <im.shovonhasan@gmail.com>
2018-07-11 20:36:20 -0400

    Improve performance of blocking and unblocking single HITs.

--- src/actions/blockHit.ts
+++ src/actions/blockHit.ts
@@ -1,35 +1,58 @@
-import { HIT_BLOCKLIST_ADD, HIT_BLOCKLIST_REMOVE } from '../constants';
+import {
+  HIT_BLOCKLIST_ADD,
+  HIT_BLOCKLIST_REMOVE,
+  HIT_BLOCKLIST_ADD_MULTIPLE,
+  HIT_BLOCKLIST_REMOVE_MULTIPLE
+} from '../constants';
 import { BlockedHit, GroupId } from '../types';
 import { Set } from 'immutable';
 
 export interface BlockHit {
   readonly type: HIT_BLOCKLIST_ADD;
+  readonly data: BlockedHit;
+}
+
+export interface BlockMultipleHits {
+  readonly type: HIT_BLOCKLIST_ADD_MULTIPLE;
   readonly data: Set<BlockedHit>;
 }
 
 export interface UnblockHit {
   readonly type: HIT_BLOCKLIST_REMOVE;
+  readonly groupId: GroupId;
+}
+
+export interface UnblockMultipleHits {
+  readonly type: HIT_BLOCKLIST_REMOVE_MULTIPLE;
   readonly groupIds: Set<GroupId>;
 }
 
-export type BlockHitAction = BlockHit | UnblockHit;
+export type BlockHitAction =
+  | BlockHit
+  | BlockMultipleHits
+  | UnblockHit
+  | UnblockMultipleHits;
 
 export const blockSingleHit = (hit: BlockedHit): BlockHit => ({
   type: HIT_BLOCKLIST_ADD,
-  data: Set([hit])
+  data: hit
 });
 
 export const unblockSingleHit = (groupId: GroupId): UnblockHit => ({
   type: HIT_BLOCKLIST_REMOVE,
-  groupIds: Set([groupId])
+  groupId
 });
 
-export const blockMultipleHits = (data: Set<BlockedHit>): BlockHit => ({
-  type: HIT_BLOCKLIST_ADD,
+export const blockMultipleHits = (
+  data: Set<BlockedHit>
+): BlockMultipleHits => ({
+  type: HIT_BLOCKLIST_ADD_MULTIPLE,
   data
 });
 
-export const unblockMultipleHits = (groupIds: Set<GroupId>): UnblockHit => ({
-  type: HIT_BLOCKLIST_REMOVE,
+export const unblockMultipleHits = (
+  groupIds: Set<GroupId>
+): UnblockMultipleHits => ({
+  type: HIT_BLOCKLIST_REMOVE_MULTIPLE,
   groupIds
 });
--- src/reducers/hitBlocklist.ts
+++ src/reducers/hitBlocklist.ts
@@ -1,6 +1,11 @@
 import { BlockedHit, HitBlockMap, GroupId } from '../types';
 import { BlockHitAction } from 'actions/blockHit';
-import { HIT_BLOCKLIST_ADD, HIT_BLOCKLIST_REMOVE } from '../constants';
+import {
+  HIT_BLOCKLIST_ADD,
+  HIT_BLOCKLIST_REMOVE,
+  HIT_BLOCKLIST_ADD_MULTIPLE,
+  HIT_BLOCKLIST_REMOVE_MULTIPLE
+} from '../constants';
 import { Map } from 'immutable';
 
 const initial: HitBlockMap = Map<GroupId, BlockedHit>();
@@ -8,11 +13,15 @@ const initial: HitBlockMap = Map<GroupId, BlockedHit>();
 export default (state = initial, action: BlockHitAction): HitBlockMap => {
   switch (action.type) {
     case HIT_BLOCKLIST_ADD:
+      return state.set(action.data.groupId, action.data);
+    case HIT_BLOCKLIST_REMOVE:
+      return state.delete(action.groupId);
+    case HIT_BLOCKLIST_ADD_MULTIPLE:
       return action.data.reduce(
         (acc: HitBlockMap, cur: BlockedHit) => acc.set(cur.groupId, cur),
         state
       );
-    case HIT_BLOCKLIST_REMOVE:
+    case HIT_BLOCKLIST_REMOVE_MULTIPLE:
       return state.filterNot((blockedHit: BlockedHit) =>
         action.groupIds.has(blockedHit.groupId)
       ) as HitBlockMap;
