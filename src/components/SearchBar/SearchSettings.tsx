50bff96bef9a924136d3eb182fe2ef39374ff406
anveio <im.shovonhasan@gmail.com>
2018-07-22 22:17:59 -0400

    Remove CustomSearch field from SearchSettings component.

--- src/components/SearchBar/SearchSettings.tsx
+++ src/components/SearchBar/SearchSettings.tsx
@@ -1,24 +1,17 @@
 import * as React from 'react';
-import { connect, Dispatch } from 'react-redux';
-import {
-  Button,
-  Card,
-  FormLayout,
-  DisplayText,
-  Caption
-} from '@shopify/polaris';
+import { connect } from 'react-redux';
+import { Button, Card, FormLayout, Caption } from '@shopify/polaris';
 import { Popover, Position } from '@blueprintjs/core';
-import { SearchAction, searchRequestSingular } from '../../actions/search';
+import { searchRequestSingular } from '../../actions/search';
 import {
   ConnectedMinRewardField,
   ConnectedSearchDelayField,
-  ConnectedSortTypeField,
-  ConnectedCustomSearchField
+  ConnectedSortTypeField
 } from './SearchFields';
 import ConnectedQualifiedBox from './QualifiedCheckBox';
 import { watchForEnter } from '../../utils/watchForEnter';
 
-export interface Handlers {
+interface Handlers {
   readonly onSearch: () => void;
 }
 
@@ -29,32 +22,31 @@ class SearchSettings extends React.PureComponent<Handlers, never> {
     return (
       <Popover position={Position.BOTTOM} canEscapeKeyClose={true}>
         <Button disclosure>Settings</Button>
-        <Card.Section>
-          <div onKeyPress={this.onEnter}>
-            <FormLayout>
-              <DisplayText size="small">Edit search settings.</DisplayText>
-              <Caption>
-                Changes are saved as you type and will apply on your next
-                search.
-              </Caption>
-              <ConnectedSearchDelayField />
-              <ConnectedMinRewardField />
-              <ConnectedCustomSearchField />
-              <ConnectedSortTypeField />
-              <ConnectedQualifiedBox />
-            </FormLayout>
-          </div>
-        </Card.Section>
+        <Card title="Edit search settings">
+          <Card.Section>
+            <div onKeyPress={this.onEnter}>
+              <FormLayout>
+                <ConnectedSearchDelayField />
+                <ConnectedMinRewardField />
+                <ConnectedSortTypeField />
+                <ConnectedQualifiedBox />
+              </FormLayout>
+            </div>
+          </Card.Section>
+          <Card.Section subdued>
+            <Caption>
+              Changes are saved as you type and will apply on your next search.
+            </Caption>
+          </Card.Section>
+        </Card>
       </Popover>
     );
   }
 }
 
-const mapDispatch = (dispatch: Dispatch<SearchAction>): Handlers => ({
-  onSearch: () => {
-    dispatch(searchRequestSingular());
-  }
-});
+const mapDispatch: Handlers = {
+  onSearch: searchRequestSingular
+};
 
 export default connect(
   null,
